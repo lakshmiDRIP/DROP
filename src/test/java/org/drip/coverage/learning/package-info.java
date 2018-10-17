35ff78c797936ede89ed06f58971a81cf029b4f6
Lakshmi Krishnamurthy <lakshmimv7977@gmail.com>
2018-10-17 18:41:35 -0400

    DROP 3.78 Release Package Broadcast

---
+++ src/test/java/org/drip/coverage/learning/package-info.java
@@ -0,0 +1,8 @@
+
+/**
+ * Statistical Learning Module Code Coverage Estimation Suite
+ *
+ * @author Lakshmi Krishnamurthy
+ */
+
+package org.drip.coverage.learning;
---
+++ src/test/java/org/drip/coverage/quant/package-info.java
@@ -0,0 +1,8 @@
+
+/**
+ * Quant Module Code Coverage Estimation Suite
+ *
+ * @author Lakshmi Krishnamurthy
+ */
+
+package org.drip.coverage.quant;
