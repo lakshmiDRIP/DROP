84d0ecdda7ac6748a9e5d7663e0f68037152e23f
Lakshmi Krishnamurthy <lakshmimv7977@gmail.com>
2018-10-19 12:03:06 -0400

    Features:
    Bug Fixes/Clean-up:
    
    Samples:
    
    	- Credit Qualifying Bucket Curvature Margin Flow 2.1 (1)
    	- Credit Qualifying Bucket Curvature Margin 2.1 (2, 3)
    	- Credit Qualifying Curvature Margin 2.1 (4)
    	- Credit Qualifying Bucket Delta Margin Flow 2.1 (5, 6)
    	- Credit Qualifying Bucket Delta Margin 2.1 (7)
    	- Credit Qualifying Delta Margin 2.1 (8, 9)
    	- Credit Qualifying Bucket Vega Margin Flow 2.1 (10)
    	- Credit Qualifying Bucket Vega Margin 2.1 (11, 12)
    	- Credit Qualifying Vega Margin 2.1 (13)
    	- Credit Qualifying Class Margin 2.1 (14, 15)
    	- Credit Non Qualifying Bucket Delta Margin Flow 2.0 (16)
    	- Credit Non Qualifying Bucket Delta Margin Flow 2.1 (17, 18)
    	- Credit Non Qualifying Bucket Vega Margin Flow 2.0 (19)
    	- Credit Non Qualifying Bucket Vega Margin Flow 2.1 (20, 21)
    	- Credit Non Qualifying Bucket Curvature Margin Flow 2.0 (22)
    	- Credit Non Qualifying Bucket Curvature Margin Flow 2.1 (23, 24)
    	- Credit Non Qualifying Bucket Delta Margin 2.0 (25)
    	- Credit Non Qualifying Bucket Delta Margin 2.1 (26, 27)
    	- Credit Non Qualifying Bucket Vega Margin 2.0 (28)
    	- Credit Non Qualifying Bucket Vega Margin 2.1 (29, 30)
    	- Credit Non Qualifying Bucket Curvature Margin 2.0 (31)
    	- Credit Non Qualifying Bucket Curvature Margin 2.1 (32, 33)
    	- Credit Non Qualifying Delta Margin 2.0 (34)
    	- Credit Non Qualifying Delta Margin 2.1 (35, 36)
    	- Credit Non Qualifying Vega Margin 2.0 (37)
    	- Credit Non Qualifying Vega Margin 2.1 (38, 39)
    	- Credit Non Qualifying Curvature Margin 2.0 (40)
    	- Credit Non Qualifying Curvature Margin 2.1 (41, 42)
    	- Credit Non Qualifying Class Margin 2.0 (43)
    	- Credit Non Qualifying Class Margin 2.1 (44, 45)

---
+++ src/main/java/org/drip/sample/simmcrq/CreditQualifyingBucketDeltaMarginFlow21.java
@@ -0,0 +1,294 @@
+
+package org.drip.sample.simmcrq;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.drip.analytics.support.CaseInsensitiveHashMap;
+import org.drip.quant.common.FormatUtil;
+import org.drip.service.env.EnvManager;
+import org.drip.simm.margin.BucketAggregateCR;
+import org.drip.simm.margin.RiskMeasureAggregateCR;
+import org.drip.simm.parameters.BucketSensitivitySettingsCR;
+import org.drip.simm.product.BucketSensitivityCR;
+import org.drip.simm.product.RiskFactorTenorSensitivity;
+
+/*
+ * -*- mode: java; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*-
+ */
+
+/*!
+ * Copyright (C) 2018 Lakshmi Krishnamurthy
+ * 
+ *  This file is part of DRIP, a free-software/open-source library for buy/side financial/trading model
+ *  	libraries targeting analysts and developers
+ *  	https://lakshmidrip.github.io/DRIP/
+ *  
+ *  DRIP is composed of four main libraries:
+ *  
+ *  - DRIP Fixed Income - https://lakshmidrip.github.io/DRIP-Fixed-Income/
+ *  - DRIP Asset Allocation - https://lakshmidrip.github.io/DRIP-Asset-Allocation/
+ *  - DRIP Numerical Optimizer - https://lakshmidrip.github.io/DRIP-Numerical-Optimizer/
+ *  - DRIP Statistical Learning - https://lakshmidrip.github.io/DRIP-Statistical-Learning/
+ * 
+ *  - DRIP Fixed Income: Library for Instrument/Trading Conventions, Treasury Futures/Options,
+ *  	Funding/Forward/Overnight Curves, Multi-Curve Construction/Valuation, Collateral Valuation and XVA
+ *  	Metric Generation, Calibration and Hedge Attributions, Statistical Curve Construction, Bond RV
+ *  	Metrics, Stochastic Evolution and Option Pricing, Interest Rate Dynamics and Option Pricing, LMM
+ *  	Extensions/Calibrations/Greeks, Algorithmic Differentiation, and Asset Backed Models and Analytics.
+ * 
+ *  - DRIP Asset Allocation: Library for model libraries for MPT framework, Black Litterman Strategy
+ *  	Incorporator, Holdings Constraint, and Transaction Costs.
+ * 
+ *  - DRIP Numerical Optimizer: Library for Numerical Optimization and Spline Functionality.
+ * 
+ *  - DRIP Statistical Learning: Library for Statistical Evaluation and Machine Learning.
+ * 
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *   	you may not use this file except in compliance with the License.
+ *   
+ *  You may obtain a copy of the License at
+ *  	http://www.apache.org/licenses/LICENSE-2.0
+ *  
+ *  Unless required by applicable law or agreed to in writing, software
+ *  	distributed under the License is distributed on an "AS IS" BASIS,
+ *  	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  
+ *  See the License for the specific language governing permissions and
+ *  	limitations under the License.
+ */
+
+/**
+ * CreditQualifyingBucketDeltaMarginFlow21 illustrates the Steps in the Computation of the SIMM 2.1 Credit
+ *  Qualifying Delta Margin for a single CR Bucket's Exposure Sensitivities. The References are:
+ *  
+ *  - Andersen, L. B. G., M. Pykhtin, and A. Sokol (2017): Credit Exposure in the Presence of Initial Margin,
+ *  	https://papers.ssrn.com/sol3/papers.cfm?abstract_id=2806156, eSSRN.
+ *  
+ *  - Albanese, C., S. Caenazzo, and O. Frankel (2017): Regression Sensitivities for Initial Margin
+ *  	Calculations, https://papers.ssrn.com/sol3/papers.cfm?abstract_id=2763488, eSSRN.
+ *  
+ *  - Anfuso, F., D. Aziz, P. Giltinan, and K. Loukopoulus (2017): A Sound Modeling and Back-testing
+ *  	Framework for Forecasting Initial Margin Requirements,
+ *  	https://papers.ssrn.com/sol3/papers.cfm?abstract_id=2716279, eSSRN.
+ *  
+ *  - Caspers, P., P. Giltinan, R. Lichters, and N. Nowaczyk (2017): Forecasting Initial Margin Requirements
+ *  	- A Model Evaluation https://papers.ssrn.com/sol3/papers.cfm?abstract_id=2911167, eSSRN.
+ *  
+ *  - International Swaps and Derivatives Association (2017): SIMM v2.0 Methodology,
+ *  	https://www.isda.org/a/oFiDE/isda-simm-v2.pdf.
+ * 
+ * @author Lakshmi Krishnamurthy
+ */
+
+public class CreditQualifyingBucketDeltaMarginFlow21
+{
+
+	private static final void AddTenorSensitivity (
+		final Map<String, Double> tenorSensitivityMap,
+		final double notional,
+		final String tenor)
+		throws Exception
+	{
+		if (tenorSensitivityMap.containsKey (tenor))
+		{
+			tenorSensitivityMap.put (
+				tenor,
+				tenorSensitivityMap.get (tenor) + notional * (Math.random() - 0.5)
+			);
+		}
+		else
+		{
+			tenorSensitivityMap.put (
+				tenor,
+				notional * (Math.random() - 0.5)
+			);
+		}
+	}
+
+	private static final RiskFactorTenorSensitivity CurveTenorSensitivityMap (
+		final double notional)
+		throws Exception
+	{
+		Map<String, Double> tenorSensitivityMap = new HashMap<String, Double>();
+
+		AddTenorSensitivity (
+			tenorSensitivityMap,
+			notional,
+			"1Y"
+		);
+
+		AddTenorSensitivity (
+			tenorSensitivityMap,
+			notional,
+			"2Y"
+		);
+
+		AddTenorSensitivity (
+			tenorSensitivityMap,
+			notional,
+			"3Y"
+		);
+
+		AddTenorSensitivity (
+			tenorSensitivityMap,
+			notional,
+			"5Y"
+		);
+
+		AddTenorSensitivity (
+			tenorSensitivityMap,
+			notional,
+			"10Y"
+		);
+
+		return new RiskFactorTenorSensitivity (tenorSensitivityMap);
+	}
+
+	private static final void DisplayComponentTenorSensitivity (
+		final String componentName,
+		final RiskFactorTenorSensitivity tenorSensitivityMap)
+		throws Exception
+	{
+		System.out.println();
+
+		System.out.println ("\t|--------------||");
+
+		System.out.println ("\t|  " + componentName + " DELTA   ||");
+
+		System.out.println ("\t|--------------||");
+
+		System.out.println ("\t|              ||");
+
+		System.out.println ("\t|  L -> R:     ||");
+
+		System.out.println ("\t|    - Tenor   ||");
+
+		System.out.println ("\t|    - Delta   ||");
+
+		System.out.println ("\t|--------------||");
+
+		for (Map.Entry<String, Double> tenorSensitivityEntry :
+			tenorSensitivityMap.sensitivityMap().entrySet())
+		{
+			System.out.println (
+				"\t| " +
+				tenorSensitivityEntry.getKey() + " => " +
+				FormatUtil.FormatDouble (tenorSensitivityEntry.getValue(), 2, 2, 1.) + " ||"
+			);
+		}
+
+		System.out.println ("\t|--------------||");
+
+		System.out.println();
+	}
+
+	private static final void ComponentRiskFactorTenorSensitivity (
+		final Map<String, RiskFactorTenorSensitivity> tenorSensitivityMap,
+		final double notional,
+		final String componentName)
+		throws Exception
+	{
+		RiskFactorTenorSensitivity ustRiskFactorSensitivity = CurveTenorSensitivityMap (notional);
+
+		tenorSensitivityMap.put (
+			componentName,
+			ustRiskFactorSensitivity
+		);
+
+		DisplayComponentTenorSensitivity (
+			componentName,
+			ustRiskFactorSensitivity
+		);
+	}
+
+	private static final void DisplayRiskMeasureAggregate (
+		final RiskMeasureAggregateCR riskMeasureAggregateCR)
+		throws Exception
+	{
+		System.out.println ("\t||--------------------------------------------||");
+
+		System.out.println ("\t||   CR RISK CLASS AGGREGATE MARGIN METRICS   ||");
+
+		System.out.println ("\t||--------------------------------------------||");
+
+		System.out.println (
+			"\t|| Core Delta SBA Variance     => " +
+			FormatUtil.FormatDouble (riskMeasureAggregateCR.coreSBAVariance(), 10, 0, 1.) + " ||"
+		);
+
+		System.out.println (
+			"\t|| Residual Delta SBA Variance => " +
+			FormatUtil.FormatDouble (riskMeasureAggregateCR.residualSBAVariance(), 10, 0, 1.) + " ||"
+		);
+
+		System.out.println (
+			"\t|| Delta SBA                   => " +
+			FormatUtil.FormatDouble (riskMeasureAggregateCR.sba(), 10, 0, 1.) + " ||"
+		);
+
+		System.out.println ("\t||--------------------------------------------||");
+
+		System.out.println();
+	}
+
+	public static final void main (
+		final String[] inputs)
+		throws Exception
+	{
+		EnvManager.InitEnv ("");
+
+		int bucketIndex = 1;
+		double notional = 100.;
+		String[] componentNameArray = new String[]
+		{
+			"UST",
+			"BND",
+			"FRT",
+			"ITA",
+			"ESP",
+			"GLT",
+		};
+
+		Map<String, RiskFactorTenorSensitivity> tenorSensitivityMap = new
+			CaseInsensitiveHashMap<RiskFactorTenorSensitivity>();
+
+		for (String componentName : componentNameArray)
+		{
+			ComponentRiskFactorTenorSensitivity (
+				tenorSensitivityMap,
+				notional,
+				componentName
+			);
+		}
+
+		BucketSensitivityCR bucketSensitivityCR = new BucketSensitivityCR (tenorSensitivityMap);
+
+		DisplayComponentTenorSensitivity (
+			"NET",
+			bucketSensitivityCR.cumulativeTenorSensitivityMap()
+		);
+
+		BucketSensitivitySettingsCR bucketSensitivitySettingsCR =
+			BucketSensitivitySettingsCR.ISDA_CRQ_DELTA_21 (bucketIndex);
+
+		BucketAggregateCR bucketAggregateCR = bucketSensitivityCR.aggregate (bucketSensitivitySettingsCR);
+
+		Map<String, BucketAggregateCR> bucketAggregateCRMap = new HashMap<String, BucketAggregateCR>();
+
+		bucketAggregateCRMap.put (
+			"" + bucketIndex,
+			bucketAggregateCR
+		);
+
+		RiskMeasureAggregateCR riskMeasureAggregateCR = new RiskMeasureAggregateCR (
+			bucketAggregateCRMap,
+			bucketAggregateCR.sensitivityMarginVariance(),
+			0.
+		);
+
+		DisplayRiskMeasureAggregate (riskMeasureAggregateCR);
+
+		EnvManager.TerminateEnv();
+	}
+}
