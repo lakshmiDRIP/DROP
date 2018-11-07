4b995af674933bdc45168dc61df48ab7fb67dc81
Lakshmi Krishnamurthy <lakshmimv7977@gmail.com>
2018-11-07 10:13:18 -0500

    Features:
    	- Exposure Evolver Dynamics Container (1)
    	- Exposure Evolver Entity Dynamics Container (2)
    	- Exposure Evolver Equity (3)
    	- Exposure Evolver Latent State Dynamics Container (4)
    	- Exposure Evolver Latent State Vertex Container (5)
    	- Exposure Evolver Primary Security (6)
    	- Exposure Evolver Primary Security Dynamics Container (7)
    	- Exposure Evolver Scaling Numeriare (8)
    	- Exposure Evolver Terminal Latent State (9)
    	- Exposure Evolver Read Me (10)
    	- Exposure Generator Fixed Stream MPoR (11)
    	- Exposure Generator Fix Float MPoR (12)
    	- Exposure Generator Float Stream MPoR (13)
    	- Exposure Generator Numeraire MPoR (14)
    	- Exposure Generator Portfolio MPoR (15)
    	- Exposure Generator Stream MPoR (16)
    	- Exposure Generator Read Me (17)
    	- Exposure Holdings Fix Float Basel Position Estimator (18)
    	- Exposure Holdings Position Group (19)
    	- Exposure Holdings Position Group Container (20)
    	- Exposure Holdings Position Group Estimator (21)
    	- Exposure Holdings Position Group Segment (22)
    	- Exposure Holdings Read Me (23)
    	- Exposure MPoR Collateral Amount Estimator (24)
    	- Exposure MPoR Collateral Amount Estimator Output (25)
    	- Exposure MPoR Margin Period Of Risk (26)
    	- Exposure MPoR Path Variation Margin Trajectory Estimator (27)
    	- Exposure MPoR Trade Payment (28)
    	- Exposure MPoR Variation Margin Trade Payment Vertex (29)
    	- Exposure MPoR Variation Margin Trade Vertex Exposure (30)
    	- Exposure MPoR Variation Margin Trajectory Builder (31)
    	- Exposure MPoR Read Me (32)
    	- Exposure Regression Andersen Pykhtin Sokol Segment (33)
    	- Exposure Regression Andersen Pykhtin Sokol Stretch (34)
    	- Exposure Regression Local Volatility Generation Control (35)
    	- Exposure Regression Pillar Vertex (36)
    	- Exposure Regression Pykhtin Brownian Bridge Segment (37)
    	- Exposure Regression Pykhtin Brownian Bridge Stretch (38)
    	- Exposure Regression Pykhtin Pillar (39)
    	- Exposure Regression Pykhtin Pillar Dynamics (40)
    	- Exposure Regression Read Me (41)
    	- Exposure Regression Trade Adjusted Variation Margin Dynamics (42)
    	- Exposure Regression Trade Adjusted Variation Margin Estimate (43)
    	- Exposure Regression Trade Adjusted Variation Margin Estimator (44)
    	- Exposure Regression Trade Andersen Pykhtin Sokol Ensemble (45)
    	- Exposure Regression Trade Andersen Pykhtin Sokol Path (46)
    	- Exposure Regression Trade Andersen Pykhtin Sokol Trajectory (47)
    	- Exposure Regression Trade Margin Estimate Vertex (48)
    	- Exposure Regression Trade Read Me (49)
    	- Exposure Universe Latent State Weiner (50)
    	- Exposure Universe Market Correlation (51)
    	- Exposure Universe Market Edge (52)
    	- Exposure Universe Market Path (53)
    	- Exposure Universe Market Vertex (54)
    	- Exposure Universe Market Vertex Entity (55)
    	- Exposure Universe Market Vertex Generator (56)
    	- Exposure Universe Read Me (57)
    	- Exposure Read Me (58)
    
    
    Bug Fixes/Clean-up:
    
    Samples:

--- src/main/java/org/drip/exposure/generator/README.md
+++ src/main/java/org/drip/exposure/generator/README.md
@@ -10,9 +10,46 @@ DROP Exposure Generator Package generates the Rates Stream Margin Period Exposur
  <i>FixedStreamMPoR</i> estimates the MPoR Variation Margin and the Trade Payments for the given Fixed Coupon
  Stream off of the Realized Market Path.
 
+ * [***FixFloatMPoR***](https://github.com/lakshmiDRIP/DROP/tree/master/src/main/java/org/drip/exposure/generator/FixFloatMPoR.java)
+ <i>FixFloatMPoR</i> estimates the MPoR Variation Margin and the Trade Payments for the given Fix Float
+ Component off of the Realized Market Path.
+
+ * [***FloatStreamMPoR***](https://github.com/lakshmiDRIP/DROP/tree/master/src/main/java/org/drip/exposure/generator/FloatStreamMPoR.java)
+ <i>FloatStreamMPoR</i> estimates the MPoR Variation Margin and the Trade Payments for the given Float Stream
+ off of the Realized Market Path.
+
+ * [***NumeraireMPoR***](https://github.com/lakshmiDRIP/DROP/tree/master/src/main/java/org/drip/exposure/generator/NumeraireMPoR.java)
+ <i>NumeraireMPoR</i> estimates the MPoR Variation Margin and the Trade Payments for the generic Numeraire
+ off of the Realized Market Path.
+
+ * [***PortfolioMPoR***](https://github.com/lakshmiDRIP/DROP/tree/master/src/main/java/org/drip/exposure/generator/PortfolioMPoR.java)
+ <i>PortfolioMPoR</i> estimates the MPoR Variation Margin and the Trade Payments for the Component MPoRs of a
+ given Portfolio off of the Realized Market Path.
+
+ * [***StreamMPoR***](https://github.com/lakshmiDRIP/DROP/tree/master/src/main/java/org/drip/exposure/generator/StreamMPoR.java)
+ <i>StreamMPoR</i> estimates the MPoR Variation Margin and the Trade Payments for the generic Stream off of
+ the Realized Market Path.
+
 
 # References
 
+ * Andersen, L. B. G., M. Pykhtin, and A. Sokol (2017): Re-thinking Margin Period of Risk
+ https://papers.ssrn.com/sol3/papers.cfm?abstract_id=2902737 <b>eSSRN</b>
+
+ * Andersen, L. B. G., M. Pykhtin, and A. Sokol (2017): Credit Exposure in the Presence of Initial Margin
+ https://papers.ssrn.com/sol3/papers.cfm?abstract_id=2806156 <b>eSSRN</b>
+
+ * Albanese, C., and L. Andersen (2014): Accounting for OTC Derivatives: Funding Adjustments and the Re-
+ Hypothecation Option https://papers.ssrn.com/sol3/papers.cfm?abstract_id=2482955 <b>eSSRN</b>
+
+ * Burgard, C., and M. Kjaer (2017): Derivatives Funding, Netting, and Accounting
+ https://papers.ssrn.com/sol3/papers.cfm?abstract_id=2534011 <b>eSSRN</b>
+
+ * Piterbarg, V. (2010): Funding Beyond Discounting: Collateral Agreements and Derivatives Pricing
+ <i>Risk</i> <b>21 (2)</b> 97-102
+
+ * Pykhtin, M. (2009): Modeling Counter-party Credit Exposure in the Presence of Margin Agreements
+ http://www.risk-europe.com/protected/michael-pykhtin.pdf
 
 
 ## DROP Specifications
--- src/main/java/org/drip/exposure/universe/MarketVertexGenerator.java
+++ src/main/java/org/drip/exposure/universe/MarketVertexGenerator.java
@@ -6,32 +6,50 @@ package org.drip.exposure.universe;
  */
 
 /*!
+ * Copyright (C) 2019 Lakshmi Krishnamurthy
  * Copyright (C) 2018 Lakshmi Krishnamurthy
  * Copyright (C) 2017 Lakshmi Krishnamurthy
  * 
- *  This file is part of DRIP, a free-software/open-source library for buy/side financial/trading model
- *  	libraries targeting analysts and developers
- *  	https://lakshmidrip.github.io/DRIP/
+ *  This file is part of DROP, an open-source library targeting risk, transaction costs, exposure, margin
+ *  	calculations, and portfolio construction within and across fixed income, credit, commodity, equity,
+ *  	FX, and structured products.
  *  
- *  DRIP is composed of four main libraries:
+ *  	https://lakshmidrip.github.io/DROP/
  *  
- *  - DRIP Fixed Income - https://lakshmidrip.github.io/DRIP-Fixed-Income/
- *  - DRIP Asset Allocation - https://lakshmidrip.github.io/DRIP-Asset-Allocation/
- *  - DRIP Numerical Optimizer - https://lakshmidrip.github.io/DRIP-Numerical-Optimizer/
- *  - DRIP Statistical Learning - https://lakshmidrip.github.io/DRIP-Statistical-Learning/
+ *  DROP is composed of three main modules:
+ *  
+ *  - DROP Analytics Core - https://lakshmidrip.github.io/DROP-Analytics-Core/
+ *  - DROP Portfolio Core - https://lakshmidrip.github.io/DROP-Portfolio-Core/
+ *  - DROP Numerical Core - https://lakshmidrip.github.io/DROP-Numerical-Core/
  * 
- *  - DRIP Fixed Income: Library for Instrument/Trading Conventions, Treasury Futures/Options,
- *  	Funding/Forward/Overnight Curves, Multi-Curve Construction/Valuation, Collateral Valuation and XVA
- *  	Metric Generation, Calibration and Hedge Attributions, Statistical Curve Construction, Bond RV
- *  	Metrics, Stochastic Evolution and Option Pricing, Interest Rate Dynamics and Option Pricing, LMM
- *  	Extensions/Calibrations/Greeks, Algorithmic Differentiation, and Asset Backed Models and Analytics.
+ * 	DROP Analytics Core implements libraries for the following:
+ * 	- Fixed Income Analytics
+ * 	- Asset Backed Analytics
+ * 	- XVA Analytics
+ * 	- Exposure and Margin Analytics
  * 
- *  - DRIP Asset Allocation: Library for model libraries for MPT framework, Black Litterman Strategy
- *  	Incorporator, Holdings Constraint, and Transaction Costs.
+ * 	DROP Portfolio Core implements libraries for the following:
+ * 	- Asset Allocation Analytics
+ * 	- Transaction Cost Analytics
  * 
- *  - DRIP Numerical Optimizer: Library for Numerical Optimization and Spline Functionality.
+ * 	DROP Numerical Core implements libraries for the following:
+ * 	- Statistical Learning Library
+ * 	- Numerical Optimizer Library
+ * 	- Machine Learning Library
+ * 	- Spline Builder Library
  * 
- *  - DRIP Statistical Learning: Library for Statistical Evaluation and Machine Learning.
+ * 	Documentation for DROP is Spread Over:
+ * 
+ * 	- Main                     => https://lakshmidrip.github.io/DROP/
+ * 	- Wiki                     => https://github.com/lakshmiDRIP/DROP/wiki
+ * 	- GitHub                   => https://github.com/lakshmiDRIP/DROP
+ * 	- Javadoc                  => https://lakshmidrip.github.io/DROP/Javadoc/index.html
+ * 	- Technical Specifications => https://github.com/lakshmiDRIP/DROP/tree/master/Docs/Internal
+ * 	- Release Versions         => https://lakshmidrip.github.io/DROP/version.html
+ * 	- Community Credits        => https://lakshmidrip.github.io/DROP/credits.html
+ * 	- Issues Catalog           => https://github.com/lakshmiDRIP/DROP/issues
+ * 	- JUnit                    => https://lakshmidrip.github.io/DROP/junit/index.html
+ * 	- Jacoco                   => https://lakshmidrip.github.io/DROP/jacoco/index.html
  * 
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *   	you may not use this file except in compliance with the License.
@@ -48,22 +66,38 @@ package org.drip.exposure.universe;
  */
 
 /**
- * MarketVertexGenerator generates the Market Realizations at a Trajectory Vertex needed for computing the
- *  Valuation Adjustment. The References are:<br><br>
+ * <i>MarketVertexGenerator</i> generates the Market Realizations at a Trajectory Vertex needed for computing
+ *  the Valuation Adjustment. The References are:
  *  
- *  - Burgard, C., and M. Kjaer (2013): Funding Strategies, Funding Costs <i>Risk</i> <b>24 (12)</b>
- *  	82-87.<br><br>
- *  
- *  - Burgard, C., and M. Kjaer (2014): PDE Representations of Derivatives with Bilateral Counter-party Risk
- *  	and Funding Costs <i>Journal of Credit Risk</i> <b>7 (3)</b> 1-19.<br><br>
- *  
- *  - Burgard, C., and M. Kjaer (2014): In the Balance <i>Risk</i> <b>24 (11)</b> 72-75.<br><br>
- *  
- *  - Gregory, J. (2009): Being Two-faced over Counter-party Credit Risk <i>Risk</i> <b>20 (2)</b>
- *  	86-90.<br><br>
- * 
- *  - Piterbarg, V. (2010): Funding Beyond Discounting: Collateral Agreements and Derivatives Pricing
- *  	<i>Risk</i> <b>21 (2)</b> 97-102.<br><br>
+ *  <br>
+ *  	<ul>
+ *  		<li>
+ *  			Burgard, C., and M. Kjaer (2013): Funding Costs, Funding Strategies <i>Risk</i> <b>23
+ *  				(12)</b> 82-87
+ *  		</li>
+ *  		<li>
+ *  			Burgard, C., and M. Kjaer (2014): In the Balance <i>Risk</i> <b>24 (11)</b> 72-75
+ *  		</li>
+ *  		<li>
+ *  			Burgard, C., and M. Kjaer (2014): PDE Representations of Derivatives with Bilateral Counter-
+ *  				party Risk and Funding Costs <i>Journal of Credit Risk</i> <b>7 (3)</b> 1-19
+ *  		</li>
+ *  		<li>
+ *  			Gregory, J. (2009): Being Two-faced over Counter-party Credit Risk <i>Risk</i> <b>20 (2)</b>
+ *  				86-90
+ *  		</li>
+ *  		<li>
+ *  			Piterbarg, V. (2010): Funding Beyond Discounting: Collateral Agreements and Derivatives
+ *  				Pricing <i>Risk</i> <b>21 (2)</b> 97-102
+ *  		</li>
+ *  		<li>
+ *  	</ul>
+ * 	<br>
+ *  <ul>
+ *		<li><b>Module</b>        = <a href = "https://github.com/lakshmiDRIP/DROP/tree/master/src/main/java/org/drip/exposure">Exposure</a></li>
+ *		<li><b>Package</b>       = <a href = "https://github.com/lakshmiDRIP/DROP/tree/master/src/main/java/org/drip/exposure/universe">Universe</a></li>
+ *		<li><b>Specification</b> = <a href = "https://github.com/lakshmiDRIP/DROP/tree/master/Docs/Internal/Exposure">Exposure Analytics</a></li>
+ *  </ul>
  * 
  * @author Lakshmi Krishnamurthy
  */
