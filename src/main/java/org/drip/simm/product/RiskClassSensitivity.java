47870e84f8c3216c9021ee970486ba3919c62332
Lakshmi Krishnamurthy <lakshmimv7977@gmail.com>
2018-12-07 04:20:45 -0500

    Features:
    	- SIMM FX FX Risk Group (1)
    	- SIMM FX FX Risk Threshold Container 2.0 (2)
    	- SIMM FX FX Risk Threshold Container 2.1 (3)
    	- SIMM FX FX Systemics 2.0 (4)
    	- SIMM FX FX Systemics 2.1 (5)
    	- SIMM FX Read Me (6)
    	- SIMM Margin Bucket Aggregate (7)
    	- SIMM Margin Bucket Aggregate CR (8)
    	- SIMM Margin Bucket Aggregate IR (9)
    	- SIMM Margin Risk Class Aggregate (10)
    	- SIMM Margin Risk Class Aggregate CR (11)
    	- SIMM Margin Risk Class Aggregate IR (12)
    	- SIMM Margin Risk Factor Aggregate (13)
    	- SIMM Margin Risk Factor Aggregate CR (14)
    	- SIMM Margin Risk Factor Aggregate IR (15)
    	- SIMM Margin Risk Measure Aggregate (16)
    	- SIMM Margin Risk Measure Aggregate CR (17)
    	- SIMM Margin Risk Measure Aggregate IR (18)
    	- SIMM Margin Sensitivity Aggregate CR (19)
    	- SIMM Margin Sensitivity Aggregate IR (20)
    	- SIMM Margin Read Me (21)
    	- SIMM Parameters Bucket Curvature Settings (22)
    	- SIMM Parameters Bucket Curvature Settings CR (23)
    	- SIMM Parameters Bucket Curvature Settings IR (24)
    	- SIMM Parameters Bucket Sensitivity Settings (25)
    	- SIMM Parameters Bucket Sensitivity Settings CR (26)
    	- SIMM Parameters Bucket Sensitivity Settings IR (27)
    	- SIMM Parameters Bucket Vega Settings (28)
    	- SIMM Parameters Bucket Vega Settings CR (29)
    	- SIMM Parameters Bucket Vega Settings IR (30)
    	- SIMM Parameters Bucket Liquidity Settings (31)
    	- SIMM Parameters Bucket Risk Class Sensitivity Settings (32)
    	- SIMM Parameters Bucket Risk Class Sensitivity Settings CR (33)
    	- SIMM Parameters Bucket Risk Class Sensitivity Settings IR (34)
    	- SIMM Parameters Bucket Risk Measure Sensitivity Settings (35)
    	- SIMM Parameters Bucket Risk Measure Sensitivity Settings CR (36)
    	- SIMM Parameters Bucket Risk Measure Sensitivity Settings IR (37)
    	- SIMM Parameters Read Me (38)
    	- SIMM Product Bucket Sensitivity (39)
    	- SIMM Product Bucket Sensitivity CR (40)
    	- SIMM Product Bucket Sensitivity IR (41)
    	- SIMM Product Credit Entity (42)
    	- SIMM Product Risk Class Sensitivity (43)
    	- SIMM Product Risk Class Sensitivity CR (44)
    	- SIMM Product Risk Class Sensitivity IR (45)
    	- SIMM Product Risk Factor Tenor Sensitivity (46)
    	- SIMM Product Risk Measure Sensitivity (47)
    	- SIMM Product Risk Measure Sensitivity CR (48)
    	- SIMM Product Risk Measure Sensitivity IR (49)
    	- SIMM Product Read Me (50)
    	- SIMM Rates Currency Risk Group (51)
    	- SIMM Rates IR Settings Container 2.0 (52)
    	- SIMM Rates IR Settings Container 2.1 (53)
    	- SIMM Rates IR Systemics (54)
    	- SIMM Rates IR Systemics 2.0 (55)
    	- SIMM Rates IR Systemics 2.1 (56)
    	- SIMM Rates IR Threshold (57)
    	- SIMM Rates IR Threshold Container 2.0 (58)
    	- SIMM Rates IR Threshold Container 2.1 (59)
    	- SIMM Rates IR Weight (60)
    	- SIMM Rates Read Me (61)
    	- SIMM Read Me (62)
    
    
    Bug Fixes/Clean-up:
    
    Samples:

--- src/main/java/org/drip/simm/product/RiskClassSensitivity.java
+++ src/main/java/org/drip/simm/product/RiskClassSensitivity.java
@@ -96,9 +96,10 @@ package org.drip.simm.product;
  * 
  * <br><br>
  *  <ul>
- *		<li><b>Project</b>       = <a href = "https://github.com/lakshmiDRIP/DROP/tree/master/src/main/java/org/drip/simm">SIMM</a></li>
- *		<li><b>Package</b>       = <a href = "https://github.com/lakshmiDRIP/DROP/tree/master/src/main/java/org/drip/simm/product">Product</a></li>
- *		<li><b>Specification</b> = <a href = "https://github.com/lakshmiDRIP/DROP/tree/master/Docs/Internal/NumericalOptimizer">Numerical Optimizer Library</a></li>
+ *		<li><b>Module </b> = <a href = "https://github.com/lakshmiDRIP/DROP/tree/master/AnalyticsCore.md">Analytics Core Module</a></li>
+ *		<li><b>Library</b> = <a href = "https://github.com/lakshmiDRIP/DROP/tree/master/ExposureAnalyticsLibrary.md">Exposure Analytics Library</a></li>
+ *		<li><b>Project</b> = <a href = "https://github.com/lakshmiDRIP/DROP/tree/master/src/main/java/org/drip/simm">SIMM</a></li>
+ *		<li><b>Package</b> = <a href = "https://github.com/lakshmiDRIP/DROP/tree/master/src/main/java/org/drip/simm/product">Product</a></li>
  *  </ul>
  * <br><br>
  * 
