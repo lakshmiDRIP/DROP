2dad56cdc74ed99376936adc8c42bcc6047da8a6
Lakshmi Krishnamurthy <lakshmimv7977@gmail.com>
2018-12-09 23:14:22 -0500

    Features:
    	- Template IRS AUD (1)
    	- Template IRS CAD (2)
    	- Template IRS CHF (3)
    	- Template IRS CNY (4)
    	- Template IRS DKK (5)
    	- Template IRS EUR (6)
    	- Template IRS GBP (7)
    	- Template IRS HKD (8)
    	- Template IRS INR (9)
    	- Template IRS JPY LIBOR (10)
    	- Template IRS JPY TIBOR (11)
    	- Template IRS KRW (12)
    	- Template IRS MYR (13)
    	- Template IRS NOK (14)
    	- Template IRS NZD (15)
    	- Template IRS PLN (16)
    	- Template IRS SEK (17)
    	- Template IRS SGD (18)
    	- Template IRS THB (19)
    	- Template IRS TWD (20)
    	- Template IRS USD (21)
    	- Template IRS ZAR (22)
    	- Template IRS Read Me (23)
    	- Template State Derived Forward State (24)
    	- Template State Forward Volatility State (25)
    	- Template State Funding State (26)
    	- Template State FX State (27)
    	- Template State Govvie State (28)
    	- Template State Overnight State (29)
    	- Template State Reference Forward State (30)
    	- Template State Survival Recovery State (31)
    	- Template State Read Me (32)
    	- Template State Bump Derived Forward State Shifted (33)
    	- Template State Bump Forward Volatility State Shifted (34)
    	- Template State Bump Funding State Shifted (35)
    	- Template State Bump FX State Shifted (36)
    	- Template State Bump Govvie State Shifted (37)
    	- Template State Bump Overnight State Shifted (38)
    	- Template State Bump Reference Forward State Shifted (39)
    	- Template State Bump Survival Recovery State Shifted (40)
    	- Template UST FV1 05Y (41)
    	- Template UST TU1 02Y (42)
    	- Template UST TY1 10Y (43)
    	- Template UST US1 30Y (44)
    	- Template UST WN1 ULTRA (45)
    	- Template UST Read Me (46)
    	- Template Read Me (47)
    	- XVA Basel Balance Sheet Edge (48)
    	- XVA Basel Balance Sheet Vertex (49)
    	- XVA Basel OTC Accounting Modus (50)
    	- XVA Basel OTC Accounting Modus FCA FBA (51)
    	- XVA Basel OTC Accounting Modus FVA FDA (52)
    	- XVA Basel OTC Accounting Policy (53)
    	- XVA Basel Value Adjustment (54)
    	- XVA Basel Value Category (55)
    	- XVA Basel Read Me (56)
    	- XVA Definition Close Out (57)
    	- XVA Definition Close Out Bilateral (58)
    	- XVA Definition PDE Evolution Control (59)
    	- XVA Definition Simple Balance Sheet (60)
    	- XVA Definition Read Me (61)
    
    
    Bug Fixes/Clean-up:
    
    Samples:

--- src/main/java/org/drip/template/statebump/GovvieStateShifted.java
+++ src/main/java/org/drip/template/statebump/GovvieStateShifted.java
@@ -83,9 +83,10 @@ import org.drip.state.govvie.GovvieCurve;
  *
  *  <br><br>
  *  <ul>
- *		<li><b>Project</b>       = <a href = "https://github.com/lakshmiDRIP/DROP/tree/master/src/main/java/org/drip/template">Template</a></li>
- *		<li><b>Package</b>       = <a href = "https://github.com/lakshmiDRIP/DROP/tree/master/src/main/java/org/drip/template/statebump">State Bump</a></li>
- *		<li><b>Specification</b> = <a href = "https://github.com/lakshmiDRIP/DROP/tree/master/Docs/Internal/FixedIncome">Fixed Income Analytics Library</a></li>
+ *		<li><b>Module </b> = <a href = "https://github.com/lakshmiDRIP/DROP/tree/master/AnalyticsCore.md">Analytics Core Module</a></li>
+ *		<li><b>Library</b> = <a href = "https://github.com/lakshmiDRIP/DROP/tree/master/FixedIncomeAnalyticsLibrary.md">Fixed Income Analytics Library</a></li>
+ *		<li><b>Project</b> = <a href = "https://github.com/lakshmiDRIP/DROP/tree/master/src/main/java/org/drip/template">Template</a></li>
+ *		<li><b>Package</b> = <a href = "https://github.com/lakshmiDRIP/DROP/tree/master/src/main/java/org/drip/template/statebump">State Bump</a></li>
  *  </ul>
  * <br><br>
  * 
