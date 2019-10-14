ee1bb80b04751efe5db1362fb630c6783a1b9cbd
Lakshmi Krishnamurthy <lakshmimv7977@gmail.com>
2019-10-13 21:13:44 -0400

    Features:
    Bug Fixes/Re-organizations:
    
    	- Analytics Day Count NL/365 (1, 2, 3)
    	- Analytics Day Count NL/Act (4, 5, 6)
    	- Analytics Definition Curve (7, 8, 9)
    	- Analytics Definition Explicit Boot Curve (10, 11, 12)
    	- Analytics Definition Latent State Static (13, 14, 15)
    	- Analytics Definition Market Surface (16, 17, 18)
    	- Analytics Definition Node Structure (19, 20, 21)
    	- Analytics Definition Turn (22, 23, 24)
    	- Analytics Event Day Base (25, 26, 27)
    	- Analytics Event Day Date In Month (28, 29, 30)
    	- Analytics Event Day Fixed (31, 32, 33)
    	- Analytics Event Day Locale (34, 35, 36)
    	- Analytics Event Day Static (37, 38, 39)
    	- Analytics Event Day Variable (40, 41, 42)
    	- Analytics Event Day Weekend (43, 44, 45)
    	- Analytics Holset Location Holiday (46, 47, 48)
    	- Analytics Input Boot Curve Construction Input (49, 50, 51)
    	- Analytics Input Curve Construction Input Set (52, 53, 54)
    	- Analytics Input Latent State Shape Preserving CCIS (55, 56, 57)
    	- Analytics Output Basket Measures (58, 59, 60)
    
    
    Samples:

--- src/main/java/org/drip/analytics/definition/MarketSurface.java
+++ src/main/java/org/drip/analytics/definition/MarketSurface.java
@@ -6,40 +6,52 @@ package org.drip.analytics.definition;
  */
 
 /*!
+ * Copyright (C) 2020 Lakshmi Krishnamurthy
  * Copyright (C) 2019 Lakshmi Krishnamurthy
  * Copyright (C) 2018 Lakshmi Krishnamurthy
  * Copyright (C) 2017 Lakshmi Krishnamurthy
  * Copyright (C) 2016 Lakshmi Krishnamurthy
  * Copyright (C) 2015 Lakshmi Krishnamurthy
  * Copyright (C) 2014 Lakshmi Krishnamurthy
+ * Copyright (C) 2013 Lakshmi Krishnamurthy
  * 
- *  This file is part of DROP, an open-source library targeting risk, transaction costs, exposure, margin
- *  	calculations, valuation adjustment, and portfolio construction within and across fixed income,
- *  	credit, commodity, equity, FX, and structured products.
+ *  This file is part of DROP, an open-source library targeting analytics/risk, transaction cost analytics,
+ *  	asset liability management analytics, capital, exposure, and margin analytics, valuation adjustment
+ *  	analytics, and portfolio construction analytics within and across fixed income, credit, commodity,
+ *  	equity, FX, and structured products. It also includes auxiliary libraries for algorithm support,
+ *  	numerical analysis, numerical optimization, spline builder, model validation, statistical learning,
+ *  	and computational support.
  *  
  *  	https://lakshmidrip.github.io/DROP/
  *  
  *  DROP is composed of three modules:
  *  
- *  - DROP Analytics Core - https://lakshmidrip.github.io/DROP-Analytics-Core/
+ *  - DROP Product Core - https://lakshmidrip.github.io/DROP-Product-Core/
  *  - DROP Portfolio Core - https://lakshmidrip.github.io/DROP-Portfolio-Core/
- *  - DROP Numerical Core - https://lakshmidrip.github.io/DROP-Numerical-Core/
+ *  - DROP Computational Core - https://lakshmidrip.github.io/DROP-Computational-Core/
  * 
- * 	DROP Analytics Core implements libraries for the following:
+ * 	DROP Product Core implements libraries for the following:
  * 	- Fixed Income Analytics
- * 	- Asset Backed Analytics
- * 	- XVA Analytics
- * 	- Exposure and Margin Analytics
+ * 	- Loan Analytics
+ * 	- Transaction Cost Analytics
  * 
  * 	DROP Portfolio Core implements libraries for the following:
  * 	- Asset Allocation Analytics
- * 	- Transaction Cost Analytics
+ *  - Asset Liability Management Analytics
+ * 	- Capital Estimation Analytics
+ * 	- Exposure Analytics
+ * 	- Margin Analytics
+ * 	- XVA Analytics
  * 
- * 	DROP Numerical Core implements libraries for the following:
- * 	- Statistical Learning Library
- * 	- Numerical Optimizer Library
- * 	- Machine Learning Library
- * 	- Spline Builder Library
+ * 	DROP Computational Core implements libraries for the following:
+ * 	- Algorithm Support
+ * 	- Computation Support
+ * 	- Function Analysis
+ *  - Model Validation
+ * 	- Numerical Analysis
+ * 	- Numerical Optimizer
+ * 	- Spline Builder
+ *  - Statistical Learning
  * 
  * 	Documentation for DROP is Spread Over:
  * 
@@ -75,10 +87,10 @@ package org.drip.analytics.definition;
  *
  *	<br><br>
  *  <ul>
- *		<li><b>Module </b> = <a href = "https://github.com/lakshmiDRIP/DROP/tree/master/AnalyticsCore.md">Analytics Core Module</a></li>
+ *		<li><b>Module </b> = <a href = "https://github.com/lakshmiDRIP/DROP/tree/master/ProductCore.md">Product Core Module</a></li>
  *		<li><b>Library</b> = <a href = "https://github.com/lakshmiDRIP/DROP/tree/master/FixedIncomeAnalyticsLibrary.md">Fixed Income Analytics</a></li>
- *		<li><b>Project</b> = <a href = "https://github.com/lakshmiDRIP/DROP/tree/master/src/main/java/org/drip/analytics/README.md">Analytics</a></li>
- *		<li><b>Package</b> = <a href = "https://github.com/lakshmiDRIP/DROP/tree/master/src/main/java/org/drip/analytics/definition/README.md">Definition</a></li>
+ *		<li><b>Project</b> = <a href = "https://github.com/lakshmiDRIP/DROP/tree/master/src/main/java/org/drip/analytics/README.md">Date, Cash Flow, and Cash Flow Period Measure Generation Utilities</a></li>
+ *		<li><b>Package</b> = <a href = "https://github.com/lakshmiDRIP/DROP/tree/master/src/main/java/org/drip/analytics/definition/README.md">Latent State Curves, Surfaces, Turns</a></li>
  *  </ul>
  *
  * @author Lakshmi Krishnamurthy
