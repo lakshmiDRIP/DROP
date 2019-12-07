b3bdd20e54605b3f4d5782475d37197f712794d8
Lakshmi Krishnamurthy <lakshmimv7977@gmail.com>
2019-12-07 02:57:54 -0500

    Features:
    Bug Fixes/Re-organization:
    
    	- XVA Hypothecation Collateral Group Vertex Exposure Component (1, 2, 3)
    	- XVA Netting Collateral Group Path (4, 5, 6)
    	- XVA Netting Credit Debt Group Path (7, 8, 9)
    	- XVA Netting Funding Group Path (10, 11, 12)
    	- XVA PDE Burgard Kjaer Edge (13, 14, 15)
    	- XVA PDE Burgard Kjaer Edge Attribution (16, 17, 18)
    	- XVA PDE Burgard Kjaer Edge Run (19, 20, 21)
    	- XVA PDE Burgard Kjaer Operator (22, 23, 24)
    	- XVA PDE Parabolic Differential Operator (25, 26, 27)
    	- XVA PDE Trajectory Evolution Scheme (28, 29, 30)
    	- XVA Proto Collateral Group Specification (31, 32, 33)
    	- XVA Proto Credit Debt Group Specification (34, 35, 36)
    	- XVA Proto Funcing Group Specification (37, 38, 39)
    	- XVA Proto Object Specification (40, 41, 42)
    	- XVA Proto Position Group Specification (43, 44, 45)
    	- XVA Proto Position Schema Specification (46, 47, 48)
    	- XVA Settings Adjustment Digest Scheme (49, 50, 51)
    	- XVA Settings Broken Date Scheme (52, 53, 54)
    	- XVA Settings Close Out Scheme (55, 56, 57)
    	- XVA Settings Position Replication Scheme (58, 59, 60)
    
    
    Samples:

---
+++ src/main/java/org/drip/dynamics/kolmogorov/README.md
@@ -0,0 +1,54 @@
+# DROP Dynamics Kolmogorov Package
+
+<p align="center"><img src="https://github.com/lakshmiDRIP/DROP/blob/master/DRIP_Logo.gif?raw=true" width="100"></p>
+
+DROP Dynamics Kolmogorov Package implements Fokker Planck Kolmogorov Forward/Backward Equations.
+
+
+## Class Components
+
+ * [***R1FokkerPlanck***](https://github.com/lakshmiDRIP/DROP/tree/master/src/main/java/org/drip/dynamics/kolmogorov/R1FokkerPlanck.java)
+ <i>R1FokkerPlanck</i> exposes the R<sup>1</sup> Fokker-Planck Probability Density Function Evolution Equation.
+
+ * [***R1FokkerPlanckBrownian***](https://github.com/lakshmiDRIP/DROP/tree/master/src/main/java/org/drip/dynamics/kolmogorov/R1FokkerPlanckBrownian.java)
+ <i>R1FokkerPlanckBrownian</i> exposes the R<sup>1</sup> Brownian Probability Density Function Evolution Equation.
+
+ * [***R1FokkerPlanckCIR***](https://github.com/lakshmiDRIP/DROP/tree/master/src/main/java/org/drip/dynamics/kolmogorov/R1FokkerPlanckCIR.java)
+ <i>R1FokkerPlanckCIR</i> exposes the R<sup>1</sup> Cox-Ingersoll-Ross Probability Density Function Evolution Equation.
+
+ * [***R1FokkerPlanckCKLS***](https://github.com/lakshmiDRIP/DROP/tree/master/src/main/java/org/drip/dynamics/kolmogorov/R1FokkerPlanckCKLS.java)
+ <i>R1FokkerPlanckCKLS</i> exposes the R<sup>1</sup> Chan-Karolyi-Longstaff-Sanders 1992 Probability Density Function Evolution Equation.
+
+ * [***R1FokkerPlanckOrnsteinUhlenbeck***](https://github.com/lakshmiDRIP/DROP/tree/master/src/main/java/org/drip/dynamics/kolmogorov/R1FokkerPlanckOrnsteinUhlenbeck.java)
+ <i>R1FokkerPlanckOrnsteinUhlenbeck</i> exposes the R<sup>1</sup> Ornstein-Uhlenbeck Probability Density Function Evolution Equation.
+
+ * [***RdFokkerPlanck***](https://github.com/lakshmiDRIP/DROP/tree/master/src/main/java/org/drip/dynamics/kolmogorov/RdFokkerPlanck.java)
+ <i>RdFokkerPlanck</i> exposes the R<sup>d</sup> Fokker-Planck Probability Density Function Evolution Equation.
+
+
+# References
+
+ * Bogoliubov, N. N., and D. P. Sankevich (1994): N. N. Bogoliubov and Statistical Mechanics <i>Russian Mathematical Surveys</i> <b>49 (5)</b> 19-49
+
+ * Holubec, V., K. Kroy, and S. Steffenoni (2019): Physically Consistent Numerical Solver for Time-dependent Fokker-Planck Equations <i>Physical Review E</i> <b>99 (4)</b> 032117
+
+ * Kadanoff, L. P. (2000): <i>Statistical Physics: Statics, Dynamics, and Re-normalization</i> <b>World Scientific</b>
+
+ * Ottinger, H. C. (1996): <i>Stochastic Processes in Polymeric Fluids</i> <b>Springer-Verlag</b> Berlin-Heidelberg
+
+ * Wikipedia (2019): Fokker-Planck Equation https://en.wikipedia.org/wiki/Fokker%E2%80%93Planck_equation
+
+
+## DROP Specifications
+
+ * Main                     => https://lakshmidrip.github.io/DROP/
+ * Wiki                     => https://github.com/lakshmiDRIP/DROP/wiki
+ * GitHub                   => https://github.com/lakshmiDRIP/DROP
+ * Repo Layout Taxonomy     => https://github.com/lakshmiDRIP/DROP/blob/master/Taxonomy.md
+ * Javadoc                  => https://lakshmidrip.github.io/DROP/Javadoc/index.html
+ * Technical Specifications => https://github.com/lakshmiDRIP/DROP/tree/master/Docs/Internal
+ * Release Versions         => https://lakshmidrip.github.io/DROP/version.html
+ * Community Credits        => https://lakshmidrip.github.io/DROP/credits.html
+ * Issues Catalog           => https://github.com/lakshmiDRIP/DROP/issues
+ * JUnit                    => https://lakshmidrip.github.io/DROP/junit/index.html
+ * Jacoco                   => https://lakshmidrip.github.io/DROP/jacoco/index.html
--- src/main/java/org/drip/xva/settings/BrokenDateScheme.java
+++ src/main/java/org/drip/xva/settings/BrokenDateScheme.java
@@ -6,42 +6,54 @@ package org.drip.xva.settings;
  */
 
 /*!
+ * Copyright (C) 2020 Lakshmi Krishnamurthy
  * Copyright (C) 2019 Lakshmi Krishnamurthy
  * Copyright (C) 2018 Lakshmi Krishnamurthy
  * 
- *  This file is part of DROP, an open-source library targeting risk, transaction costs, exposure, margin
- *  	calculations, and portfolio construction within and across fixed income, credit, commodity, equity,
- *  	FX, and structured products.
+ *  This file is part of DROP, an open-source library targeting analytics/risk, transaction cost analytics,
+ *  	asset liability management analytics, capital, exposure, and margin analytics, valuation adjustment
+ *  	analytics, and portfolio construction analytics within and across fixed income, credit, commodity,
+ *  	equity, FX, and structured products. It also includes auxiliary libraries for algorithm support,
+ *  	numerical analysis, numerical optimization, spline builder, model validation, statistical learning,
+ *  	and computational support.
  *  
  *  	https://lakshmidrip.github.io/DROP/
  *  
- *  DROP is composed of three main modules:
+ *  DROP is composed of three modules:
  *  
- *  - DROP Analytics Core - https://lakshmidrip.github.io/DROP-Analytics-Core/
+ *  - DROP Product Core - https://lakshmidrip.github.io/DROP-Product-Core/
  *  - DROP Portfolio Core - https://lakshmidrip.github.io/DROP-Portfolio-Core/
- *  - DROP Numerical Core - https://lakshmidrip.github.io/DROP-Numerical-Core/
+ *  - DROP Computational Core - https://lakshmidrip.github.io/DROP-Computational-Core/
  * 
- * 	DROP Analytics Core implements libraries for the following:
+ * 	DROP Product Core implements libraries for the following:
  * 	- Fixed Income Analytics
- * 	- Asset Backed Analytics
- * 	- XVA Analytics
- * 	- Exposure and Margin Analytics
+ * 	- Loan Analytics
+ * 	- Transaction Cost Analytics
  * 
  * 	DROP Portfolio Core implements libraries for the following:
  * 	- Asset Allocation Analytics
- * 	- Transaction Cost Analytics
+ *  - Asset Liability Management Analytics
+ * 	- Capital Estimation Analytics
+ * 	- Exposure Analytics
+ * 	- Margin Analytics
+ * 	- XVA Analytics
  * 
- * 	DROP Numerical Core implements libraries for the following:
- * 	- Statistical Learning Library
- * 	- Numerical Optimizer Library
- * 	- Machine Learning Library
- * 	- Spline Builder Library
+ * 	DROP Computational Core implements libraries for the following:
+ * 	- Algorithm Support
+ * 	- Computation Support
+ * 	- Function Analysis
+ *  - Model Validation
+ * 	- Numerical Analysis
+ * 	- Numerical Optimizer
+ * 	- Spline Builder
+ *  - Statistical Learning
  * 
  * 	Documentation for DROP is Spread Over:
  * 
  * 	- Main                     => https://lakshmidrip.github.io/DROP/
  * 	- Wiki                     => https://github.com/lakshmiDRIP/DROP/wiki
  * 	- GitHub                   => https://github.com/lakshmiDRIP/DROP
+ * 	- Repo Layout Taxonomy     => https://github.com/lakshmiDRIP/DROP/blob/master/Taxonomy.md
  * 	- Javadoc                  => https://lakshmidrip.github.io/DROP/Javadoc/index.html
  * 	- Technical Specifications => https://github.com/lakshmiDRIP/DROP/tree/master/Docs/Internal
  * 	- Release Versions         => https://lakshmidrip.github.io/DROP/version.html
@@ -94,10 +106,10 @@ package org.drip.xva.settings;
  *
  *  <br><br>
  *  <ul>
- *		<li><b>Module </b> = <a href = "https://github.com/lakshmiDRIP/DROP/tree/master/AnalyticsCore.md">Analytics Core Module</a></li>
+ *		<li><b>Module </b> = <a href = "https://github.com/lakshmiDRIP/DROP/tree/master/PortfolioCore.md">Portfolio Core Module</a></li>
  *		<li><b>Library</b> = <a href = "https://github.com/lakshmiDRIP/DROP/tree/master/XVAAnalyticsLibrary.md">XVA Analytics Library</a></li>
- *		<li><b>Project</b> = <a href = "https://github.com/lakshmiDRIP/DROP/tree/master/src/main/java/org/drip/xva">XVA</a></li>
- *		<li><b>Package</b> = <a href = "https://github.com/lakshmiDRIP/DROP/tree/master/src/main/java/org/drip/xva/settings">Settings</a></li>
+ *		<li><b>Project</b> = <a href = "https://github.com/lakshmiDRIP/DROP/tree/master/src/main/java/org/drip/xva/README.md">Valuation Adjustments that account for Collateral, CC Credit/Debt and Funding Overhead</a></li>
+ *		<li><b>Package</b> = <a href = "https://github.com/lakshmiDRIP/DROP/tree/master/src/main/java/org/drip/xva/settings/README.md">XVA Group and Path Settings</a></li>
  *  </ul>
  * <br><br>
  * 
