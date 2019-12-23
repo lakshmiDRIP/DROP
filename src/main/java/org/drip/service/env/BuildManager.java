afc850e046490aace36cabb3a24420676fc17959
Lakshmi Krishnamurthy <lakshmimv7977@gmail.com>
2019-12-23 13:04:30 -0500

    Features:
    Bug Fixes/Re-organization:
    
    Samples:
    
    	- Bessel First Frobenius Estimate (1, 2, 3, 4)
    	- Bessel First Schalfli Integer Estimate (5, 6, 7, 8)
    	- Bessel First Schlafli Non-Integer Estimate (9, 10, 11, 12)
    	- Bessel High Z First Estimate (13, 14, 15, 16)
    	- Bessel High Z Second Estimate (17, 18, 19, 20)
    	- Bessel Modified First Alpha Half Asymptote (21, 22, 23, 24)
    	- Bessel Modified First Frobenius Estimate (25, 26, 27, 28)
    	- Bessel Modified First Hankel Estimate (29, 30, 31, 32)
    	- Bessel Modified First Integral Estimate (33, 34, 35, 36)
    	- Bessel Modified Second Alpha Half Asymptote (37, 38, 39, 40)
    	- Bessel Modified Second Hankel Asymptote (41, 42, 43, 44)
    	- Bessel Modified Second Integral Estimate (45, 46, 47, 48)
    	- Bessel Modified Second One Third Order (49, 50, 51, 52)
    	- Bessel Modified Second Two Third Order (53, 54, 55, 56)
    	- Bessel Modified Second Zero Order (57, 58, 59, 60)

--- src/main/java/org/drip/service/env/BuildManager.java
+++ src/main/java/org/drip/service/env/BuildManager.java
@@ -109,6 +109,11 @@ public class BuildManager
 		try {
 			s_aBuildRecord = new org.drip.service.env.BuildRecord[] {
 				new org.drip.service.env.BuildRecord (
+					"4.56.0",
+					"1.8.0_112",
+					"Mon Dec 23 12:03:47 EST 2019"
+				),
+				new org.drip.service.env.BuildRecord (
 					"4.55.0",
 					"1.8.0_112",
 					"Mon Dec 23 04:05:35 EST 2019"
