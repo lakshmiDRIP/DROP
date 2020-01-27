9bdc7554860b02985d0b3156086160c2c87ad4f5
Lakshmi Krishnamurthy <lakshmimv7977@gmail.com>
2020-01-26 23:52:55 -0500

    Features:
    Bug Fixes/Re-organization:
    
    Samples:
    
    	- Dual CAD 3M 6M USD 3M 6M (1, 2, 3, 4)
    	- Dual CCBS Discount Curve (5, 6, 7, 8)
    	- Dual CCBS Forward Curve (9, 10, 11, 12)
    	- Dual CHF 3M 6M USD 3M 6M (13, 14, 15, 16)
    	- Dual DKK 3M 6M USD 3M 6M (17, 18, 19, 20)
    	- Dual EUR 3M 6M USD 3M 6M (21, 22, 23, 24)
    	- Dual GBP 3M 6M USD 3M 6M (25, 26, 27, 28)
    	- Dual JPY 3M 6M USD 3M 6M (29, 30, 31, 32)
    	- Dual NOK 3M 6M USD 3M 6M (33, 34, 35, 36)
    	- Dual PLN 3M 6M USD 3M 6M (37, 38, 39, 40)
    	- Dual SEK 3M 6M USD 3M 6M (41, 42, 43, 44)
    	- Efficient Frontier Bounded Markovitz Bullet (45, 46, 47, 48)
    	- Efficient Frontier Long Only Markovitz Bullet (49, 50, 51, 52)
    	- Efficient Frontier Unbounded Markovitz Bullet (53, 54, 55, 56)
    	- Efficient Frontier Unbounded Markovitz Bullet Explicit (57, 58, 59, 60)

--- src/main/java/org/drip/service/env/BuildManager.java
+++ src/main/java/org/drip/service/env/BuildManager.java
@@ -109,6 +109,11 @@ public class BuildManager
 		try {
 			s_aBuildRecord = new org.drip.service.env.BuildRecord[] {
 				new org.drip.service.env.BuildRecord (
+					"4.66.0",
+					"1.8.0_112",
+					"Sun Jan 26 23:00:02 EST 2020"
+				),
+				new org.drip.service.env.BuildRecord (
 					"4.65.0",
 					"1.8.0_112",
 					"Fri Jan 24 23:32:00 EST 2020"
