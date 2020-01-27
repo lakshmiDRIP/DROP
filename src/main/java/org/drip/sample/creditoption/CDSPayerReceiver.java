52cef6128671501e1e9c1a7c98dd5ce1515e3ff9
Lakshmi Krishnamurthy <lakshmimv7977@gmail.com>
2020-01-26 19:32:56 -0500

    Features:
    Bug Fixes/Re-organization:
    
    Samples:
    
    	- Digamma Saddle Point Estimate (1, 2, 3, 4)
    	- Digamma Taylor Riemann Zeta Estimate (5, 6, 7, 8)
    	- Digamma Unit Imaginary Estimate (9, 10, 11, 12)
    	- Digamma Zero One Bound Property (13, 14, 15, 16)
    	- Digamma Zero To One Estimate (17, 18, 19, 20)
    	- Distance Test Exponential Andersen Darling Gap Analysis (21, 22, 23, 24)
    	- Distance Test Exponential Andersen Darling Gap Discriminant (25, 26, 27, 28)
    	- Distance Test Exponential Cramers von Mises Gap Analysis (29, 30, 31, 32)
    	- Distance Test Exponential Cramers von Mises Gap Discriminant (33, 34, 35, 36)
    	- Distance Test Normal Andersen Darling Gap Analysis (37, 38, 39, 40)
    	- Distance Test Normal Andersen Darling Gap Discriminant (41, 42, 43, 44)
    	- Distance Test Normal Cramers von Mises Gap Analysis (45, 46, 47, 48)
    	- Distance Test Normal Cramers von Mises Gap Discriminant (49, 50, 51, 52)
    	- Distance Test Uniform Andersen Darling Gap Analysis (53, 54, 55, 56)
    	- Distance Test Uniform Andersen Darling Gap Discriminant (57, 58, 59, 60)

--- src/main/java/org/drip/sample/creditoption/CDSPayerReceiver.java
+++ src/main/java/org/drip/sample/creditoption/CDSPayerReceiver.java
@@ -26,38 +26,49 @@ import org.drip.state.identifier.*;
  */
 
 /*!
+ * Copyright (C) 2020 Lakshmi Krishnamurthy
  * Copyright (C) 2019 Lakshmi Krishnamurthy
  * Copyright (C) 2018 Lakshmi Krishnamurthy
  * Copyright (C) 2017 Lakshmi Krishnamurthy
  * Copyright (C) 2016 Lakshmi Krishnamurthy
  * 
- *  This file is part of DROP, an open-source library targeting risk, transaction costs, exposure, margin
- *  	calculations, valuation adjustment, and portfolio construction within and across fixed income,
- *  	credit, commodity, equity, FX, and structured products.
+ *  This file is part of DROP, an open-source library targeting analytics/risk, transaction cost analytics,
+ *  	asset liability management analytics, capital, exposure, and margin analytics, valuation adjustment
+ *  	analytics, and portfolio construction analytics within and across fixed income, credit, commodity,
+ *  	equity, FX, and structured products. It also includes auxiliary libraries for algorithm support,
+ *  	numerical analysis, numerical optimization, spline builder, model validation, statistical learning,
+ *  	and computational support.
  *  
  *  	https://lakshmidrip.github.io/DROP/
  *  
  *  DROP is composed of three modules:
  *  
- *  - DROP Analytics Core - https://lakshmidrip.github.io/DROP-Analytics-Core/
+ *  - DROP Product Core - https://lakshmidrip.github.io/DROP-Product-Core/
  *  - DROP Portfolio Core - https://lakshmidrip.github.io/DROP-Portfolio-Core/
- *  - DROP Numerical Core - https://lakshmidrip.github.io/DROP-Numerical-Core/
+ *  - DROP Computational Core - https://lakshmidrip.github.io/DROP-Computational-Core/
  * 
- * 	DROP Analytics Core implements libraries for the following:
+ * 	DROP Product Core implements libraries for the following:
  * 	- Fixed Income Analytics
- * 	- Asset Backed Analytics
- * 	- XVA Analytics
- * 	- Exposure and Margin Analytics
+ * 	- Loan Analytics
+ * 	- Transaction Cost Analytics
  * 
  * 	DROP Portfolio Core implements libraries for the following:
  * 	- Asset Allocation Analytics
- * 	- Transaction Cost Analytics
+ *  - Asset Liability Management Analytics
+ * 	- Capital Estimation Analytics
+ * 	- Exposure Analytics
+ * 	- Margin Analytics
+ * 	- XVA Analytics
  * 
- * 	DROP Numerical Core implements libraries for the following:
- * 	- Statistical Learning
+ * 	DROP Computational Core implements libraries for the following:
+ * 	- Algorithm Support
+ * 	- Computation Support
+ * 	- Function Analysis
+ *  - Model Validation
+ * 	- Numerical Analysis
  * 	- Numerical Optimizer
  * 	- Spline Builder
- * 	- Algorithm Support
+ *  - Statistical Learning
  * 
  * 	Documentation for DROP is Spread Over:
  * 
@@ -92,10 +103,10 @@ import org.drip.state.identifier.*;
  *  
  * <br><br>
  *  <ul>
- *		<li><b>Module </b> = <a href = "https://github.com/lakshmiDRIP/DROP/tree/master/AnalyticsCore.md">Analytics Core Module</a></li>
- *		<li><b>Library</b> = <a href = "https://github.com/lakshmiDRIP/DROP/tree/master/FixedIncomeAnalyticsLibrary.md">Fixed Income Analytics Library</a></li>
- *		<li><b>Project</b> = <a href = "https://github.com/lakshmiDRIP/DROP/tree/master/src/main/java/org/drip/sample/README.md">Sample</a></li>
- *		<li><b>Package</b> = <a href = "https://github.com/lakshmiDRIP/DROP/tree/master/src/main/java/org/drip/sample/creditoption/README.md">CDS Option Analytics</a></li>
+ *		<li><b>Module </b> = <a href = "https://github.com/lakshmiDRIP/DROP/tree/master/ProductCore.md">Product Core Module</a></li>
+ *		<li><b>Library</b> = <a href = "https://github.com/lakshmiDRIP/DROP/tree/master/FixedIncomeAnalyticsLibrary.md">Fixed Income Analytics</a></li>
+ *		<li><b>Project</b> = <a href = "https://github.com/lakshmiDRIP/DROP/tree/master/src/main/java/org/drip/sample/README.md">DROP API Construction and Usage</a></li>
+ *		<li><b>Package</b> = <a href = "https://github.com/lakshmiDRIP/DROP/tree/master/src/main/java/org/drip/sample/creditoption/README.md">CDS Single Name Index Option</a></li>
  *  </ul>
  * <br><br>
  * 
