698fed4d595d920532f8b49fa92433ef999682d8
Lakshmi Krishnamurthy <lakshmimv7977@gmail.com>
2020-04-24 07:19:43 -0400

    h series samples on

---
+++ src/main/java/org/drip/sample/heap/package-info.java
@@ -0,0 +1,8 @@
+
+/**
+ * Priority Queue and Heap Algorithms
+ *
+ * @author Lakshmi Krishnamurthy
+ */
+
+package org.drip.sample.heap;
---
+++ src/main/java/org/drip/sample/helitterman/package-info.java
@@ -0,0 +1,8 @@
+
+/**
+ * He Litterman (1999) Projection Loadings
+ *
+ * @author Lakshmi Krishnamurthy
+ */
+
+package org.drip.sample.helitterman;
