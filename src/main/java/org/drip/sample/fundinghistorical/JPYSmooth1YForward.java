24af69f41b90d9091aafc3cb6bf3b625a9467048
Lakshmi Krishnamurthy <lakshmi7977@gmail.com>
2021-08-07 11:27:18 -0400

    Features:
    Bug Fixes/Re-organization:
    
    Samples:
    
    	- Sample Funding Feed CZK Shape Preserving Reconstitutor (1)
    	- Sample Funding Feed DKK Shape Preserving Reconstitutor (2)
    	- Sample Funding Feed EUR Shape Preserving Reconstitutor (3)
    	- Sample Funding Feed EUR Smooth Reconstitutor (4)
    	- Sample Funding Feed GBP Shape Preserving Reconstitutor (5)
    	- Sample Funding Feed GBP Smooth Reconstitutor (6)
    	- Sample Funding Feed HKD Shape Preserving Reconstitutor (7)
    	- Sample Funding Feed HUF Shape Preserving Reconstitutor (8)
    	- Sample Funding Feed ILS Shape Preserving Reconstitutor (9)
    	- Sample Funding Feed JPY Shape Preserving Reconstitutor (10)
    	- Sample Funding Feed JPY Smooth Reconstitutor (11)
    	- Sample Funding Feed MXN Shape Preserving Reconstitutor (12)
    	- Sample Funding Feed NOK Shape Preserving Reconstitutor (13)
    	- Sample Funding Feed NOK Smooth Reconstitutor (14)
    	- Sample Funding Feed NZD Shape Preserving Reconstitutor (15)
    	- Sample Funding Feed NZD Smooth Reconstitutor (16)
    	- Sample Funding Feed PLN Shape Preserving Reconstitutor (17)
    	- Sample Funding Feed SEK Shape Preserving Reconstitutor (18)
    	- Sample Funding Feed SEK Smooth Reconstitutor (19)
    	- Sample Funding Feed SGD Shape Preserving Reconstitutor (20)
    	- Sample Funding Feed TRY Shape Preserving Reconstitutor (21)
    	- Sample Funding Feed Unified Shape Preserving 1Y Start (24)
    	- Sample Funding Feed USD Shape Preserving Reconstitutor (23)
    	- Sample Funding Feed USD Smooth Reconstitutor (24)
    	- Sample Funding Feed ZAR Shape Preserving Reconstitutor (25)
    	- Sample Funding Historical AUD Shape Preserving 1Y Forward (26)
    	- Sample Funding Historical AUD Shape Preserving 1Y Start (27)
    	- Sample Funding Historical AUD Smooth 1Y Forward (28)
    	- Sample Funding Historical CAD Shape Preserving 1Y Forward (29)
    	- Sample Funding Historical CAD Shape Preserving 1Y Start (30)
    	- Sample Funding Historical CAD Smooth 1Y Forward (31)
    	- Sample Funding Historical CHF Shape Preserving 1Y Forward (32)
    	- Sample Funding Historical CHF Shape Preserving 1Y Start (33)
    	- Sample Funding Historical CHF Smooth 1Y Forward (34)
    	- Sample Funding Historical CZK Shape Preserving 1Y Start (35)
    	- Sample Funding Historical DKK Shape Preserving 1Y Start (36)
    	- Sample Funding Historical EUR Shape Preserving 1Y Forward (37)
    	- Sample Funding Historical EUR Shape Preserving 1Y Start (38)
    	- Sample Funding Historical EUR Smooth 1Y Forward (39)
    	- Sample Funding Historical GBP Shape Preserving 1Y Forward (40)
    	- Sample Funding Historical GBP Shape Preserving 1Y Start (41)
    	- Sample Funding Historical GBP Smooth 1Y Forward (42)
    	- Sample Funding Historical HKD Shape Preserving 1Y Start (43)
    	- Sample Funding Historical HUF Shape Preserving 1Y Start (44)
    	- Sample Funding Historical ILS Shape Preserving 1Y Start (45)
    	- Sample Funding Historical JPY Shape Preserving 1Y Forward (46)
    	- Sample Funding Historical JPY Shape Preserving 1Y Start (47)
    	- Sample Funding Historical JPY Smooth 1Y Forward (48)
    	- Sample Funding Historical MXN Shape Preserving 1Y Start (49)
    	- Sample Funding Historical NOK Shape Preserving 1Y Forward (50)
    	- Sample Funding Historical NOK Shape Preserving 1Y Start (51)
    	- Sample Funding Historical NOK Smooth 1Y Forward (52)
    	- Sample Funding Historical NZD Shape Preserving 1Y Forward (53)
    	- Sample Funding Historical NZD Shape Preserving 1Y Start (54)
    	- Sample Funding Historical NZD Smooth 1Y Forward (55)
    	- Sample Funding Historical PLN Shape Preserving 1Y Start (56)
    	- Sample Funding Historical SEK Shape Preserving 1Y Forward (57)
    	- Sample Funding Historical SEK Shape Preserving 1Y Start (58)
    	- Sample Funding Historical SEK Smooth 1Y Forward (59)
    	- Sample Funding Historical SGD Shape Preserving 1Y Start (60)

--- src/main/java/org/drip/sample/fundinghistorical/JPYSmooth1YForward.java
+++ src/main/java/org/drip/sample/fundinghistorical/JPYSmooth1YForward.java
@@ -16,6 +16,8 @@ import org.drip.service.template.LatentMarketStateBuilder;
  */
 
 /*!
+ * Copyright (C) 2022 Lakshmi Krishnamurthy
+ * Copyright (C) 2021 Lakshmi Krishnamurthy
  * Copyright (C) 2020 Lakshmi Krishnamurthy
  * Copyright (C) 2019 Lakshmi Krishnamurthy
  * Copyright (C) 2018 Lakshmi Krishnamurthy
@@ -27,7 +29,7 @@ import org.drip.service.template.LatentMarketStateBuilder;
  *  	analytics, and portfolio construction analytics within and across fixed income, credit, commodity,
  *  	equity, FX, and structured products. It also includes auxiliary libraries for algorithm support,
  *  	numerical analysis, numerical optimization, spline builder, model validation, statistical learning,
- *  	and computational support.
+ *  	graph builder/navigator, and computational support.
  *  
  *  	https://lakshmidrip.github.io/DROP/
  *  
@@ -54,6 +56,7 @@ import org.drip.service.template.LatentMarketStateBuilder;
  * 	- Algorithm Support
  * 	- Computation Support
  * 	- Function Analysis
+ *  - Graph Algorithm
  *  - Model Validation
  * 	- Numerical Analysis
  * 	- Numerical Optimizer
@@ -71,8 +74,6 @@ import org.drip.service.template.LatentMarketStateBuilder;
  * 	- Release Versions         => https://lakshmidrip.github.io/DROP/version.html
  * 	- Community Credits        => https://lakshmidrip.github.io/DROP/credits.html
  * 	- Issues Catalog           => https://github.com/lakshmiDRIP/DROP/issues
- * 	- JUnit                    => https://lakshmidrip.github.io/DROP/junit/index.html
- * 	- Jacoco                   => https://lakshmidrip.github.io/DROP/jacoco/index.html
  * 
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *   	you may not use this file except in compliance with the License.
