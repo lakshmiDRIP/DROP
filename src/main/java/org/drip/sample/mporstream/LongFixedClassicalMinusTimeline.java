edeb8b4b5128e7a5223c2babcb2ed573529a05b3
Lakshmi Krishnamurthy <lakshmi7977@gmail.com>
2021-08-14 10:18:55 -0400

    Features:
    Bug Fixes/Re-organization:
    
    Samples:
    
    	- Sample MPoR Fix-Float OTC Receiver Classical- Timeline (1)
    	- Sample MPoR Fix-Float OTC Receiver Classical+ Timeline (2)
    	- Sample MPoR Fix-Float OTC Receiver Conservative (3)
    	- Sample MPoR Fix-Float XVA OTC Payer CSA Aggressive (4)
    	- Sample MPoR Fix-Float XVA OTC Payer CSA Classical- (5)
    	- Sample MPoR Fix-Float XVA OTC Payer CSA Classical+ (6)
    	- Sample MPoR Fix-Float XVA OTC Payer CSA Conservative (7)
    	- Sample MPoR Fix-Float XVA OTC Receiver CSA Aggressive (8)
    	- Sample MPoR Fix-Float XVA OTC Receiver CSA Classical- (9)
    	- Sample MPoR Fix-Float XVA OTC Receiver CSA Classical+ (10)
    	- Sample MPoR Fix-Float XVA OTC Receiver CSA Conservative (11)
    	- Sample MPoR Stream Long Fixed Aggressive Timeline (12)
    	- Sample MPoR Stream Long Fixed Classical+ Timeline (13)
    	- Sample MPoR Stream Long Fixed Classical- Timeline (14)
    	- Sample MPoR Stream Long Fixed Conservative Timeline (15)
    	- Sample MPoR Stream Long Float Aggressive Timeline (16)
    	- Sample MPoR Stream Long Float Classical+ Timeline (17)
    	- Sample MPoR Stream Long Float Classical- Timeline (18)
    	- Sample MPoR Stream Long Float Conservative Timeline (19)
    	- Sample MPoR Stream Short Fixed Aggressive Timeline (20)
    	- Sample MPoR Stream Short Fixed Classical+ Timeline (21)
    	- Sample MPoR Stream Short Fixed Classical- Timeline (22)
    	- Sample MPoR Stream Short Fixed Conservative Timeline (23)
    	- Sample MPoR Stream Short Float Aggressive Timeline (24)
    	- Sample MPoR Stream Short Float Classical+ Timeline (25)
    	- Sample MPoR Stream Short Float Classical- Timeline (26)
    	- Sample MPoR Stream Short Float Conservative Timeline (27)
    	- Sample MST Boruvka Maximum Forest Generator (28)
    	- Sample MST Boruvka Minimum Forest Generator (29)
    	- Sample MST Complete Uniform Random Boruvka (30)
    	- Sample MST Complete Uniform Random Kruskal (31)
    	- Sample MST Complete Uniform Random Prim (32)
    	- Sample MST Complete Uniform Random Reverse Delete (33)
    	- Sample MST Complete Uniform Random Steele (34)
    	- Sample MST Kruskal Maximum Forest Generator (35)
    	- Sample MST Kruskal Minimum Forest Generator (36)
    	- Sample MST Prim Maximum Forest Generator (37)
    	- Sample MST Prim Minimum Forest Generator (38)
    	- Sample MST Reverse Delete Maximum Forest Generator (39)
    	- Sample MST Reverse Delete Minimum Forest Generator (40)
    	- Sample Multicurve Custom Basis Curve Builder (41)
    	- Sample Multicurve Fix Float Forward Curve (42)
    	- Sample Multicurve Fix Float Swap (43)
    	- Sample Multicurve Fix Float Swap Analysis (44)
    	- Sample Multicurve Fix Float Swap IMM (45)
    	- Sample Multicurve Float Float Forward Curve (46)
    	- Sample Multicurve Funding Native Forward Reconciler (47)
    	- Sample Multicurve OTC Swap Option Settlements (48)
    	- Sample Municipal Davengere (49)
    	- Sample Municipal Kozhikode (50)
    	- Sample Municipal Kurnool (51)
    	- Sample Municipal Fixed Bullet 1 (52)
    	- Sample Municipal Fixed Bullet 2 (53)
    	- Sample Municipal Fixed Bullet 3 (54)
    	- Sample Netting Portfolio Group Run (55)
    	- Sample Netting Portfolio Group Simulation (56)
    	- Sample Netting Portfolio Path Aggregation Correlated (57)
    	- Sample Netting Portfolio Path Aggregation Deterministic (58)
    	- Sample Netting Portfolio Path Aggregation Uncorrelated (59)
    	- Newton Cotes Arc Tangent Generalized Mid-Point (60)

--- src/main/java/org/drip/sample/mporstream/LongFixedClassicalMinusTimeline.java
+++ src/main/java/org/drip/sample/mporstream/LongFixedClassicalMinusTimeline.java
@@ -48,6 +48,8 @@ import org.drip.state.identifier.OvernightLabel;
  */
 
 /*!
+ * Copyright (C) 2022 Lakshmi Krishnamurthy
+ * Copyright (C) 2021 Lakshmi Krishnamurthy
  * Copyright (C) 2020 Lakshmi Krishnamurthy
  * Copyright (C) 2019 Lakshmi Krishnamurthy
  * Copyright (C) 2018 Lakshmi Krishnamurthy
@@ -57,7 +59,7 @@ import org.drip.state.identifier.OvernightLabel;
  *  	analytics, and portfolio construction analytics within and across fixed income, credit, commodity,
  *  	equity, FX, and structured products. It also includes auxiliary libraries for algorithm support,
  *  	numerical analysis, numerical optimization, spline builder, model validation, statistical learning,
- *  	and computational support.
+ *  	graph builder/navigator, and computational support.
  *  
  *  	https://lakshmidrip.github.io/DROP/
  *  
@@ -84,6 +86,7 @@ import org.drip.state.identifier.OvernightLabel;
  * 	- Algorithm Support
  * 	- Computation Support
  * 	- Function Analysis
+ *  - Graph Algorithm
  *  - Model Validation
  * 	- Numerical Analysis
  * 	- Numerical Optimizer
@@ -101,8 +104,6 @@ import org.drip.state.identifier.OvernightLabel;
  * 	- Release Versions         => https://lakshmidrip.github.io/DROP/version.html
  * 	- Community Credits        => https://lakshmidrip.github.io/DROP/credits.html
  * 	- Issues Catalog           => https://github.com/lakshmiDRIP/DROP/issues
- * 	- JUnit                    => https://lakshmidrip.github.io/DROP/junit/index.html
- * 	- Jacoco                   => https://lakshmidrip.github.io/DROP/jacoco/index.html
  * 
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *   	you may not use this file except in compliance with the License.
