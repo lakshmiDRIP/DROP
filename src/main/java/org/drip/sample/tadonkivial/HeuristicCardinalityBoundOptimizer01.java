f8cd452fc8c96b7ba17f53006e33fb4f03c27791
Lakshmi Krishnamurthy <lakshmi7977@gmail.com>
2021-08-22 18:37:28 -0400

    Features:
    Bug Fixes/Re-organization:
    
    Samples:
    
    	- Sample Systemic Stress Trading NORTH AMERICA (1)
    	- Sample Systemic Stress Trading LATIN AMERICA (2)
    	- Sample Tadonki Vial Heuristic Cardinality Bound Optimizer 1 (3)
    	- Sample Tadonki Vial Heuristic Cardinality Bound Optimizer 2 (4)
    	- Sample Tadonki Vial Heuristic Cardinality Bound Optimizer 3 (5)
    	- Sample Tadonki Vial Heuristic Cardinality Bound Optimizer 4 (6)
    	- Sample Tadonki Vial Heuristic Cardinality Bound Optimizer 5 (7)
    	- Sample Tadonki Vial Heuristic Cardinality Bound Optimizer 6 (8)
    	- Sample Tadonki Vial Heuristic Cardinality Bound Optimizer 7 (9)
    	- Sample Tadonki Vial Heuristic Cardinality Bound Optimizer 8 (10)
    	- Sample Tadonki Vial Heuristic Cardinality Bound Optimizer 9 (11)
    	- Sample Tadonki Vial Heuristic Cardinality Bound Optimizer 10 (12)
    	- Sample Tadonki Vial Heuristic Cardinality Bound Optimizer 11 (13)
    	- Sample Tadonki Vial Heuristic Cardinality Bound Optimizer 12 (14)
    	- Sample Tadonki Vial Heuristic Cardinality Bound Optimizer 13 (15)
    	- Sample Tadonki Vial Heuristic Cardinality Bound Optimizer 14 (16)
    	- Sample Tadonki Vial Heuristic Cardinality Bound Optimizer 15 (17)
    	- Sample Tadonki Vial Heuristic Cardinality Bound Optimizer 16 (18)
    	- Sample Tadonki Vial Heuristic Cardinality Bound Optimizer 17 (19)
    	- Sample Tadonki Vial Heuristic Cardinality Bound Optimizer 18 (20)
    	- Sample Treasury Govvie Bond Definitions (21)
    	- Sample Treasury Fixed Bullet (22)
    	- Sample Treasury YAS BTPS (23)
    	- Sample Treasury YAS CAN (24)
    	- Sample Treasury YAS DBR (25)
    	- Sample Treasury YAS FRTR (26)
    	- Sample Treasury YAS GGB (27)
    	- Sample Treasury YAS GILT (28)
    	- Sample Treasury YAS JGB (29)
    	- Sample Treasury YAS MBONO (30)
    	- Sample Treasury YAS SPGB (31)
    	- Sample Treasury YAS UST (32)
    	- Sample Treasury Feed AGB Reconstitutor (33)
    	- Sample Treasury Feed CAN Reconstitutor (34)
    	- Sample Treasury Feed DBR Reconstitutor (35)
    	- Sample Treasury Feed DGB Reconstitutor (36)
    	- Sample Treasury Feed GILT Reconstitutor (37)
    	- Sample Treasury Feed GSWISS Reconstitutor (38)
    	- Sample Treasury Feed JGB Reconstitutor (39)
    	- Sample Treasury Feed NGB Reconstitutor (40)
    	- Sample Treasury Feed NZGB Reconstitutor (41)
    	- Sample Treasury Feed SGB Reconstitutor (42)
    	- Sample Treasury Feed UST Reconstitutor (43)
    	- Sample Treasury Futures Contract Definitions (44)
    	- Sample Treasury Futures Contract Eligibility Settlement Definitions (45)
    	- Sample Treasury Futures Exchange Traded Option Definitions (46)
    	- Sample Treasury Futures Expiry Delivery Trading Dates (47)
    	- Sample Treasury Futures UST 02Y (48)
    	- Sample Treasury Futures UST 05Y (49)
    	- Sample Treasury Futures UST 10Y (50)
    	- Sample Treasury Futures UST 30Y (51)
    	- Sample Treasury Futures UST ULTRA (52)
    	- Sample Treasury Futures API CN1 (53)
    	- Sample Treasury Futures API DU1 (54)
    	- Sample Treasury Futures API FBB1 (55)
    	- Sample Treasury Futures API FV1 (56)
    	- Sample Treasury Futures API G1 (57)
    	- Sample Treasury Futures API IK1 (58)
    	- Sample Treasury Futures API JB1 (59)
    	- Sample Treasury Futures API Key Rate Duration (60)
    
    
    IdeaDRIP:

--- src/main/java/org/drip/sample/tadonkivial/HeuristicCardinalityBoundOptimizer01.java
+++ src/main/java/org/drip/sample/tadonkivial/HeuristicCardinalityBoundOptimizer01.java
@@ -18,15 +18,16 @@ import org.drip.service.env.EnvManager;
  */
 
 /*!
+ * Copyright (C) 2022 Lakshmi Krishnamurthy
+ * Copyright (C) 2021 Lakshmi Krishnamurthy
  * Copyright (C) 2020 Lakshmi Krishnamurthy
- * Copyright (C) 2019 Lakshmi Krishnamurthy
  * 
  *  This file is part of DROP, an open-source library targeting analytics/risk, transaction cost analytics,
  *  	asset liability management analytics, capital, exposure, and margin analytics, valuation adjustment
  *  	analytics, and portfolio construction analytics within and across fixed income, credit, commodity,
  *  	equity, FX, and structured products. It also includes auxiliary libraries for algorithm support,
  *  	numerical analysis, numerical optimization, spline builder, model validation, statistical learning,
- *  	and computational support.
+ *  	graph builder/navigator, and computational support.
  *  
  *  	https://lakshmidrip.github.io/DROP/
  *  
@@ -53,6 +54,7 @@ import org.drip.service.env.EnvManager;
  * 	- Algorithm Support
  * 	- Computation Support
  * 	- Function Analysis
+ *  - Graph Algorithm
  *  - Model Validation
  * 	- Numerical Analysis
  * 	- Numerical Optimizer
@@ -70,8 +72,6 @@ import org.drip.service.env.EnvManager;
  * 	- Release Versions         => https://lakshmidrip.github.io/DROP/version.html
  * 	- Community Credits        => https://lakshmidrip.github.io/DROP/credits.html
  * 	- Issues Catalog           => https://github.com/lakshmiDRIP/DROP/issues
- * 	- JUnit                    => https://lakshmidrip.github.io/DROP/junit/index.html
- * 	- Jacoco                   => https://lakshmidrip.github.io/DROP/jacoco/index.html
  * 
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *   	you may not use this file except in compliance with the License.
