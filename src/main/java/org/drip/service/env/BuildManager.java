10d3c2b19fd072d416c0cc2489957d330e405de5
Lakshmi Krishnamurthy <lakshmi7977@gmail.com>
2021-10-20 22:12:14 -0400

    Features:
    Bug Fixes/Re-organization:
    
    Samples:
    
    IdeaDRIP:
    
    	- Speech Production Places of Articulation - (1-21)
    	- Speech Production Development (22-41)
    	- Labial Consonant Overview (42-55)
    	- Labial Consonant Lack of Labials (56-60)

--- src/main/java/org/drip/service/env/BuildManager.java
+++ src/main/java/org/drip/service/env/BuildManager.java
@@ -110,6 +110,11 @@ public class BuildManager
 		try {
 			s_aBuildRecord = new org.drip.service.env.BuildRecord[] {
 				new org.drip.service.env.BuildRecord (
+					"5.18.0",
+					"15.0.1+9-18",
+					"Wed Oct 20 21:52:17 EST 2021"
+				),
+				new org.drip.service.env.BuildRecord (
 					"5.17.0",
 					"15.0.1+9-18",
 					"Tue Oct 19 21:16:30 EST 2021"
