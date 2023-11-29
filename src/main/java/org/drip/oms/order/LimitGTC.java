ef633737316cd4bb65a1f127dddea5d61714ef0f
Lakshmi Krishnamurthy <lakshmi7977@gmail.com>
2023-11-28 21:50:41 -0500

    Features:
    Bug Fixes/Re-organization:
    
    	- Limit Close Order Annotated Shell (1, 2)
    	- Limit Close Order Annotated Constructor (3, 4)
    	- Standard Limit Close Order Builder (5, 6)
    	- Standard Limit Close Order Buy (7, 8)
    	- Standard Limit Close Order Sell (9, 10)
    	- Limit DAY Order Annotated Shell (11, 12)
    	- Limit DAY Order Annotated Constructor (13, 14)
    	- Standard Limit DAY Order Builder (15, 16)
    	- Standard Limit DAY Order Buy (17, 18)
    	- Standard Limit DAY Order Sell (19, 20)
    	- Limit DTC Order Annotated Shell (21, 22)
    	- Limit DTC Order Annotated Constructor (23, 24)
    	- Standard Limit DTC Order Builder (25, 26)
    	- Standard Limit DTC Order Buy (27, 28)
    	- Standard Limit DTC Order Sell (29, 30)
    	- Limit GTC Order Annotated Shell (31, 32)
    	- Limit GTC Order Annotated Constructor (33, 34)
    	- Standard Limit GTC Order Builder (35, 36)
    	- Standard Limit GTC Order Buy (37, 38)
    	- Standard Limit GTC Order Sell (39, 40)
    	- Limit IOC Order Annotated Shell (41, 42)
    	- Limit IOC Order Annotated Constructor (43, 44)
    	- Standard Limit IOC Order Builder (45, 46)
    	- Standard Limit IOC Order Buy (47, 48)
    	- Standard Limit IOC Order Sell (49, 50)
    	- Limit Open Order Annotated Shell (51, 52)
    	- Limit Open Order Annotated Constructor (53, 54)
    	- Standard Limit Open Order Builder (55, 56)
    	- Standard Limit Open Order Buy (57, 58)
    	- Standard Limit Open Order Sell (59, 60)
    
    
    Samples:
    
    IdeaDRIP:

---
+++ src/main/java/org/drip/oms/order/LimitGTC.java
@@ -0,0 +1,299 @@
+
+package org.drip.oms.order;
+
+import java.util.Date;
+
+import org.drip.oms.transaction.Order;
+import org.drip.oms.transaction.OrderFillWholeSettings;
+import org.drip.oms.transaction.OrderIssuer;
+import org.drip.oms.transaction.TimeInForce;
+import org.drip.service.common.StringUtil;
+
+/*
+ * -*- mode: java; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*-
+ */
+
+/*!
+ * Copyright (C) 2023 Lakshmi Krishnamurthy
+ * 
+ *  This file is part of DROP, an open-source library targeting analytics/risk, transaction cost analytics,
+ *  	asset liability management analytics, capital, exposure, and margin analytics, valuation adjustment
+ *  	analytics, and portfolio construction analytics within and across fixed income, credit, commodity,
+ *  	equity, FX, and structured products. It also includes auxiliary libraries for algorithm support,
+ *  	numerical analysis, numerical optimization, spline builder, model validation, statistical learning,
+ *  	graph builder/navigator, and computational support.
+ *  
+ *  	https://lakshmidrip.github.io/DROP/
+ *  
+ *  DROP is composed of three modules:
+ *  
+ *  - DROP Product Core - https://lakshmidrip.github.io/DROP-Product-Core/
+ *  - DROP Portfolio Core - https://lakshmidrip.github.io/DROP-Portfolio-Core/
+ *  - DROP Computational Core - https://lakshmidrip.github.io/DROP-Computational-Core/
+ * 
+ * 	DROP Product Core implements libraries for the following:
+ * 	- Fixed Income Analytics
+ * 	- Loan Analytics
+ * 	- Transaction Cost Analytics
+ * 
+ * 	DROP Portfolio Core implements libraries for the following:
+ * 	- Asset Allocation Analytics
+ *  - Asset Liability Management Analytics
+ * 	- Capital Estimation Analytics
+ * 	- Exposure Analytics
+ * 	- Margin Analytics
+ * 	- XVA Analytics
+ * 
+ * 	DROP Computational Core implements libraries for the following:
+ * 	- Algorithm Support
+ * 	- Computation Support
+ * 	- Function Analysis
+ *  - Graph Algorithm
+ *  - Model Validation
+ * 	- Numerical Analysis
+ * 	- Numerical Optimizer
+ * 	- Spline Builder
+ *  - Statistical Learning
+ * 
+ * 	Documentation for DROP is Spread Over:
+ * 
+ * 	- Main                     => https://lakshmidrip.github.io/DROP/
+ * 	- Wiki                     => https://github.com/lakshmiDRIP/DROP/wiki
+ * 	- GitHub                   => https://github.com/lakshmiDRIP/DROP
+ * 	- Repo Layout Taxonomy     => https://github.com/lakshmiDRIP/DROP/blob/master/Taxonomy.md
+ * 	- Javadoc                  => https://lakshmidrip.github.io/DROP/Javadoc/index.html
+ * 	- Technical Specifications => https://github.com/lakshmiDRIP/DROP/tree/master/Docs/Internal
+ * 	- Release Versions         => https://lakshmidrip.github.io/DROP/version.html
+ * 	- Community Credits        => https://lakshmidrip.github.io/DROP/credits.html
+ * 	- Issues Catalog           => https://github.com/lakshmiDRIP/DROP/issues
+ * 
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *   	you may not use this file except in compliance with the License.
+ *   
+ *  You may obtain a copy of the License at
+ *  	http://www.apache.org/licenses/LICENSE-2.0
+ *  
+ *  Unless required by applicable law or agreed to in writing, software
+ *  	distributed under the License is distributed on an "AS IS" BASIS,
+ *  	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  
+ *  See the License for the specific language governing permissions and
+ *  	limitations under the License.
+ */
+
+/**
+ * <i>LimitGTC</i> holds the Details of a Limit GTC Order. The References are:
+ *  
+ * 	<br><br>
+ *  <ul>
+ * 		<li>
+ * 			Berkowitz, S. A., D. E. Logue, and E. A. J. Noser (1988): The Total Cost of Transactions on the
+ * 				NYSE <i>Journal of Finance</i> <b>43 (1)</b> 97-112
+ * 		</li>
+ * 		<li>
+ * 			Cont, R., and A. Kukanov (2017): Optimal Order Placement in Limit Order Markets <i>Quantitative
+ * 				Finance</i> <b>17 (1)</b> 21-39
+ * 		</li>
+ * 		<li>
+ * 			Vassilis, P. (2005a): A Realistic Model of Market Liquidity and Depth <i>Journal of Futures
+ * 				Markets</i> <b>25 (5)</b> 443-464
+ * 		</li>
+ * 		<li>
+ * 			Vassilis, P. (2005b): Slow and Fast Markets <i>Journal of Economics and Business</i> <b>57
+ * 				(6)</b> 576-593
+ * 		</li>
+ * 		<li>
+ * 			Weiss, D. (2006): <i>After the Trade is Made: Processing Securities Transactions</i> <b>Portfolio
+ * 				Publishing</b> London UK
+ * 		</li>
+ *  </ul>
+ *
+ *	<br><br>
+ *  <ul>
+ *		<li><b>Module </b> = <a href = "https://github.com/lakshmiDRIP/DROP/tree/master/ProductCore.md">Product Core Module</a></li>
+ *		<li><b>Library</b> = <a href = "https://github.com/lakshmiDRIP/DROP/tree/master/TransactionCostAnalyticsLibrary.md">Transaction Cost Analytics</a></li>
+ *		<li><b>Project</b> = <a href = "https://github.com/lakshmiDRIP/DROP/tree/master/src/main/java/org/drip/oms/README.md">R<sup>d</sup> Order Specification, Handling, and Management</a></li>
+ *		<li><b>Package</b> = <a href = "https://github.com/lakshmiDRIP/DROP/tree/master/src/main/java/org/drip/oms/specification/README.md">Order Specification and Session Metrics</a></li>
+ *  </ul>
+ *
+ * @author Lakshmi Krishnamurthy
+ */
+
+public class LimitGTC
+	extends Limit
+{
+
+	/**
+	 * Create a Standard Instance of Limit GTC
+	 * 
+	 * @param issuer Order Issuer
+	 * @param securityIdentifier Security Identifier
+	 * @param side Order Side
+	 * @param size Order Size
+	 * @param durationDays Duration Days
+	 * @param fillWholeSettings Order Fill-Whole Settings
+	 * @param thresholdPrice Threshold Price
+	 * 
+	 * @return Standard Instance of Limit GTC
+	 */
+
+	public static final LimitGTC Standard (
+		final OrderIssuer issuer,
+		final String securityIdentifier,
+		final String side,
+		final double size,
+		final int durationDays,
+		final OrderFillWholeSettings fillWholeSettings,
+		final double thresholdPrice)
+	{
+		try
+		{
+			return new LimitGTC (
+				issuer,
+				securityIdentifier,
+				StringUtil.GUID(),
+				new Date(),
+				side,
+				size,
+				durationDays,
+				fillWholeSettings,
+				thresholdPrice
+			);
+		}
+		catch (Exception e)
+		{
+			e.printStackTrace();
+		}
+
+		return null;
+	}
+
+	/**
+	 * Create a Standard Instance of Buy Limit GTC
+	 * 
+	 * @param issuer Order Issuer
+	 * @param securityIdentifier Security Identifier
+	 * @param size Order Size
+	 * @param durationDays Duration Days
+	 * @param fillWholeSettings Order Fill-Whole Settings
+	 * @param thresholdPrice Threshold Price
+	 * 
+	 * @return Standard Instance of Buy Limit GTC
+	 */
+
+	public static final LimitGTC StandardBuy (
+		final OrderIssuer issuer,
+		final String securityIdentifier,
+		final double size,
+		final int durationDays,
+		final OrderFillWholeSettings fillWholeSettings,
+		final double thresholdPrice)
+	{
+		try
+		{
+			return new LimitGTC (
+				issuer,
+				securityIdentifier,
+				StringUtil.GUID(),
+				new Date(),
+				Order.BUY,
+				size,
+				durationDays,
+				fillWholeSettings,
+				thresholdPrice
+			);
+		}
+		catch (Exception e)
+		{
+			e.printStackTrace();
+		}
+
+		return null;
+	}
+
+	/**
+	 * Create a Standard Instance of Sell Limit GTC
+	 * 
+	 * @param issuer Order Issuer
+	 * @param securityIdentifier Security Identifier
+	 * @param size Order Size
+	 * @param durationDays Duration Days
+	 * @param fillWholeSettings Order Fill-Whole Settings
+	 * @param thresholdPrice Threshold Price
+	 * 
+	 * @return Standard Instance of Sell Limit GTC
+	 */
+
+	public static final LimitGTC StandardSell (
+		final OrderIssuer issuer,
+		final String securityIdentifier,
+		final double size,
+		final int durationDays,
+		final OrderFillWholeSettings fillWholeSettings,
+		final double thresholdPrice)
+	{
+		try
+		{
+			return new LimitGTC (
+				issuer,
+				securityIdentifier,
+				StringUtil.GUID(),
+				new Date(),
+				Order.SELL,
+				size,
+				durationDays,
+				fillWholeSettings,
+				thresholdPrice
+			);
+		}
+		catch (Exception e)
+		{
+			e.printStackTrace();
+		}
+
+		return null;
+	}
+
+	/**
+	 * Limit GTC Order Constructor
+	 * 
+	 * @param issuer Order Issuer
+	 * @param securityIdentifier Security Identifier
+	 * @param id Order ID
+	 * @param creationTime Creation Time
+	 * @param side Order Side
+	 * @param size Order Size
+	 * @param durationDays Duration Days
+	 * @param fillWholeSettings Order Fill-Whole Settings
+	 * @param thresholdPrice Threshold Price
+	 * 
+	 * @throws Exception Thrown if the Inputs are Invalid
+	 */
+
+	public LimitGTC (
+		final OrderIssuer issuer,
+		final String securityIdentifier,
+		final String id,
+		final Date creationTime,
+		final String side,
+		final double size,
+		final int durationDays,
+		final OrderFillWholeSettings fillWholeSettings,
+		final double thresholdPrice)
+		throws Exception
+	{
+		super (
+			issuer,
+			securityIdentifier,
+			id,
+			creationTime,
+			side,
+			size,
+			TimeInForce.CreateGoodTillCanceled (
+				durationDays
+			),
+			fillWholeSettings,
+			thresholdPrice
+		);
+	}
+}
