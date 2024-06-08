0362cc6f85689a105437dc95ec70e2eec7476ec8
Lakshmi Krishnamurthy <lakshmi7977@gmail.com>
2024-06-08 01:02:30 -0400

    Features:
    Bug Fixes/Re-organization:
    
    Samples:
    
    IdeaDRIP:
    
    	- Successive Over-relaxation Algorithm (1-42)
    	- Symmetric Successive Over-relaxation Algorithm (43-48)
    	- Successive Over-relaxation Algorithm Other Applications of the Method (49-60)

--- src/main/java/org/drip/service/env/BuildManager.java
+++ src/main/java/org/drip/service/env/BuildManager.java
@@ -110,6 +110,11 @@ public class BuildManager
 		try {
 			s_aBuildRecord = new org.drip.service.env.BuildRecord[] {
 				new org.drip.service.env.BuildRecord (
+					"6.33.0",
+					"15.0.1+9-18",
+					"Sat Jun 08 00:49:42 EST 2024"
+				),
+				new org.drip.service.env.BuildRecord (
 					"6.32.0",
 					"15.0.1+9-18",
 					"Mon Jun 03 21:21:19 EST 2024"
