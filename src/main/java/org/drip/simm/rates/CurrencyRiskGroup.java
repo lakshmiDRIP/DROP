5951697bfa2031ae76399a277989a2a1d600b26e
Lakshmi Krishnamurthy <lakshmi7977@gmail.com>
2025-08-30 14:11:37 -0400

    Features:
    	- SIMM Rates Currency Risk Group (99, 100, 101)
    	- Currency Risk Group Volatility Type #1 (102, 103)
    	- Currency Risk Group Volatility Type #2 (104, 105, 106)
    	- Currency Risk Group Trade Frequency Type #1 (107, 108)
    	- Currency Risk Group Trade Frequency Type #2 (109, 110, 111)
    	- Currency Risk Group Component Array #1 (112, 113, 114)
    	- Currency Risk Group Component Array #2 (115, 116)
    	- Currency Risk Group Constructor #1 (117, 118)
    	- Currency Risk Group Constructor #2 (119, 120)
    
    
    Bug Fixes/Re-organization:
    
    Samples:
    
    IdeaDRIP:
    
    	- The Right and Wrong Models for evaluating Callable Municipal Bonds - Risk-Neutral Versus Real Worlds – An Example (1-58)
    	- The Right and Wrong Models for evaluating Callable Municipal Bonds - Types of Term Structure Models and their Roles #1 (59-61)
    	- The Right and Wrong Models for evaluating Callable Municipal Bonds - Types of Term Structure Models and their Roles #2 (62-67)
    	- The Right and Wrong Models for evaluating Callable Municipal Bonds - Types of Term Structure Models and their Roles #3 (68-71)
    	- The Right and Wrong Models for evaluating Callable Municipal Bonds - Types of Term Structure Models and their Roles #4 (72-75)
    	- The Right and Wrong Models for evaluating Callable Municipal Bonds - Types of Term Structure Models and their Roles Deterministic #1 (76-80)
    	- The Right and Wrong Models for evaluating Callable Municipal Bonds - Types of Term Structure Models and their Roles Deterministic #2 (81-90)
    	- The Right and Wrong Models for evaluating Callable Municipal Bonds - Types of Term Structure Models and their Roles Deterministic #3 (91-94)
    	- The Right and Wrong Models for evaluating Callable Municipal Bonds - Types of Term Structure Models and their Roles Deterministic #4 (95-98)

--- src/main/java/org/drip/simm/rates/CurrencyRiskGroup.java
+++ src/main/java/org/drip/simm/rates/CurrencyRiskGroup.java
@@ -6,6 +6,9 @@ package org.drip.simm.rates;
  */
 
 /*!
+ * Copyright (C) 2025 Lakshmi Krishnamurthy
+ * Copyright (C) 2024 Lakshmi Krishnamurthy
+ * Copyright (C) 2023 Lakshmi Krishnamurthy
  * Copyright (C) 2022 Lakshmi Krishnamurthy
  * Copyright (C) 2021 Lakshmi Krishnamurthy
  * Copyright (C) 2020 Lakshmi Krishnamurthy
@@ -105,15 +108,20 @@ package org.drip.simm.rates;
  *  			https://www.isda.org/a/oFiDE/isda-simm-v2.pdf
  *  	</li>
  *  </ul>
- * 
- * <br><br>
- *  <ul>
- *		<li><b>Module </b> = <a href = "https://github.com/lakshmiDRIP/DROP/tree/master/PortfolioCore.md">Portfolio Core Module</a></li>
- *		<li><b>Library</b> = <a href = "https://github.com/lakshmiDRIP/DROP/tree/master/MarginAnalyticsLibrary.md">Initial and Variation Margin Analytics</a></li>
- *		<li><b>Project</b> = <a href = "https://github.com/lakshmiDRIP/DROP/tree/master/src/main/java/org/drip/simm/README.md">Initial Margin Analytics based on ISDA SIMM and its Variants</a></li>
- *		<li><b>Package</b> = <a href = "https://github.com/lakshmiDRIP/DROP/tree/master/src/main/java/org/drip/simm/rates/README.md">SIMM IR Risk Factor Settings</a></li>
- *  </ul>
- * <br><br>
+ *
+ *  <br>
+ *  <style>table, td, th {
+ *  	padding: 1px; border: 2px solid #008000; border-radius: 8px; background-color: #dfff00;
+ *		text-align: center; color:  #0000ff;
+ *  }
+ *  </style>
+ *  
+ *  <table style="border:1px solid black;margin-left:auto;margin-right:auto;">
+ *		<tr><td><b>Module </b></td> <td><a href = "https://github.com/lakshmiDRIP/DROP/tree/master/ComputationalCore.md">Computational Core Module</a></td></tr>
+ *		<tr><td><b>Library</b></td> <td><a href = "https://github.com/lakshmiDRIP/DROP/tree/master/StatisticalLearningLibrary.md">Statistical Learning Library</a></td></tr>
+ *		<tr><td><b>Project</b></td> <td><a href = "https://github.com/lakshmiDRIP/DROP/tree/master/src/main/java/org/drip/simm/README.md">Initial Margin Analytics based on ISDA SIMM and its Variants</a></td></tr>
+ *		<tr><td><b>Package</b></td> <td><a href = "https://github.com/lakshmiDRIP/DROP/tree/master/src/main/java/org/drip/simm/rates/README.md">SIMM IR Risk Factor Settings</a></td></tr>
+ *  </table>
  * 
  * @author Lakshmi Krishnamurthy
  */
@@ -125,7 +133,7 @@ public class CurrencyRiskGroup
 	private String[] _componentArray = null;
 
 	/**
-	 * CurrencyRiskGroup Constructor
+	 * <i>CurrencyRiskGroup</i> Constructor
 	 * 
 	 * @param volatilityType The Volatility Type
 	 * @param tradeFrequencyType The Trade Frequency Type
@@ -142,12 +150,9 @@ public class CurrencyRiskGroup
 	{
 		if (null == (_volatilityType = volatilityType) || _volatilityType.isEmpty() ||
 			null == (_tradeFrequencyType = tradeFrequencyType) || _tradeFrequencyType.isEmpty() ||
-			null == (_componentArray = componentArray) || 0 == _componentArray.length
-		)
+			null == (_componentArray = componentArray) || 0 == _componentArray.length)
 		{
-			throw new Exception (
-				"CurrencyRiskGroup Constructor => Invalid Inputs"
-			);
+			throw new Exception ("CurrencyRiskGroup Constructor => Invalid Inputs");
 		}
 	}
 
